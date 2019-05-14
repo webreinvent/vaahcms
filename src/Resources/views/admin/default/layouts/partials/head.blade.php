2989aa68d9710de6ea34d133867c42024244ba38
Pradeep Kumar <webreinvent@gmail.com>
2019-05-15 00:33:08 +0530

    vue js is included

--- src/Resources/views/admin/default/layouts/partials/head.blade.php
+++ src/Resources/views/admin/default/layouts/partials/head.blade.php
@@ -1,11 +1,11 @@
-
     <meta charset="UTF-8">
-
 
     <meta name=description content="">
 
     <meta name=viewport content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" id="_token" content="{{ csrf_token() }}">
+    <base href="{{\URL::to('/')}}">
 
 
 
--- src/Resources/views/admin/default/setup/welcome.blade.php
+++ src/Resources/views/admin/default/setup/welcome.blade.php
@@ -6,7 +6,9 @@
     <div class="content ">
 
         <div class="container ht-100p">
-            <div class="ht-100p d-flex flex-column align-items-center justify-content-center">
+
+            <!--header-->
+            <div class="d-flex flex-column align-items-center justify-content-center">
 
                 <div class="navbar-brand">
                     <a href="{{url("/")}}" class="df-logo tx-40">Vaah<span>CMS</span>
@@ -14,8 +16,8 @@
                     </a>
                 </div>
 
-                <ul class="steps">
-                    <li class="step-item">
+                <ul class="steps mg-t-50 mg-b-50">
+                    <li class="step-item active">
                         <a href="" class="step-link">
                             <span class="step-number">1</span>
                             <span class="step-title">Database</span>
@@ -35,14 +37,74 @@
                     </li>
                 </ul>
 
-                <h4 class="tx-20 tx-sm-24">Verify your email address</h4>
-                <p class="tx-color-03 mg-b-40">Please check your email and click the verify button or link to verify your account.</p>
-                <div class="tx-13 tx-lg-14 mg-b-40">
-                    <a href="" class="btn btn-brand-02 d-inline-flex align-items-center">Resend Verification</a>
-                    <a href="" class="btn btn-white d-inline-flex align-items-center mg-l-5">Contact Support</a>
-                </div>
-                <span class="tx-12 tx-color-03">Mailbox with envelope vector is created by <a href="https://www.freepik.com/free-photos-vectors/background">freepik (freepik.com)</a></span>
+
             </div>
+            <!--/header-->
+
+            <!--form-->
+            <div class="row">
+
+                <div class="col-6 offset-3">
+
+
+                    <form>
+                        <div class="form-group row">
+                            <label for="dbhost" class="col-sm-3 col-form-label">DB Host</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="dbhost" placeholder="Database Host Name">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="dbport" class="col-sm-3 col-form-label">DB Port</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="dbport" placeholder="Database Port">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label for="dbname" class="col-sm-3 col-form-label">DB Name</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="dbname" placeholder="Database Name">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="dbusername" class="col-sm-3 col-form-label">DB Username</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="dbusername" placeholder="Database Username">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="dbpassword" class="col-sm-3 col-form-label">DB Password</label>
+                            <div class="col-sm-9">
+                                <input type="password" class="form-control" id="dbpassword" placeholder="Password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mg-b-0">
+
+                            <label class="col-sm-3 col-form-label"></label>
+                            <div class="col-sm-9">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+
+                        </div>
+                    </form>
+
+                </div>
+
+
+            </div>
+            <!--/form-->
+
+            <!--footer-->
+            <div class="d-flex flex-column align-items-center justify-content-center mg-t-50">
+                <span class="tx-12 tx-color-03">VaahCMS is developed by <a href="https://www.webreinvent.com">WebReinvent</a></span>
+            </div>
+            <!--/footer-->
+            
         </div>
 
     </div>
