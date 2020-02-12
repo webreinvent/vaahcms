0632f0f5cbd4e720558f82239a375889b864d150
Pradeep <webreinvent@gmail.com>
2020-02-12 20:35:10 +0530

    backup

--- src/Resources/assets/admin/default/vue/settings/partials/AsideMenu.vue
+++ src/Resources/assets/admin/default/vue/settings/partials/AsideMenu.vue
@@ -1,5 +1,47 @@
 <template>
-    $END$
+
+
+    <div class="">
+
+        <div class="sidebar-body">
+            <ul class="sidebar-nav">
+                <li class="nav-label mg-b-15">Settings</li>
+                <li class="nav-item">
+
+                    <router-link href="/settings"
+                                 :class="{'active': $route.matched[0].path == '/settings/localization'}"
+                                 :to="{ path: '/settings/localization'}" exact
+                                 class="nav-link">
+                        Localization
+                    </router-link>
+                </li>
+
+
+
+                <!--<li class="nav-item show">
+                    <a href="" class="nav-link with-sub"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-package"><path d="M12.89 1.45l8 4A2 2 0 0 1 22 7.24v9.53a2 2 0 0 1-1.11 1.79l-8 4a2 2 0 0 1-1.79 0l-8-4a2 2 0 0 1-1.1-1.8V7.24a2 2 0 0 1 1.11-1.79l8-4a2 2 0 0 1 1.78 0z"></path><polyline points="2.32 6.16 12 11 21.68 6.16"></polyline><line x1="12" y1="22.76" x2="12" y2="11"></line><line x1="7" y1="3.5" x2="17" y2="8.5"></line></svg> Utilities</a>
+                    <nav class="nav">
+                        <a href="util-animation.html">Animation</a>
+                        <a href="util-background.html">Background</a>
+                        <a href="util-border.html">Border</a>
+                        <a href="util-display.html">Display</a>
+                        <a href="util-divider.html">Divider</a>
+                        <a href="util-flex.html">Flex</a>
+                        <a href="util-height.html">Height</a>
+                        <a href="util-margin.html">Margin</a>
+                        <a href="util-padding.html">Padding</a>
+                        <a href="util-position.html">Position</a>
+                        <a href="util-typography.html">Typography</a>
+                        <a href="util-width.html">Width</a>
+                        <a href="util-extras.html">Extras</a>
+                    </nav>
+                </li>-->
+
+            </ul>
+        </div>
+
+    </div>
+
 </template>
 
 <script>
