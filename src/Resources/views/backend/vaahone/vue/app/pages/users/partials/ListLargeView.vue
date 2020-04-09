7c72fb1c473fc24dd9e9dd4495b757dd14238619
vikram chand <vikram.wri231@webreinvent.com>
2020-04-09 19:24:59 +0530

    update list view

--- src/Resources/views/backend/vaahone/vue/app/pages/users/partials/ListLargeView.vue
+++ src/Resources/views/backend/vaahone/vue/app/pages/users/partials/ListLargeView.vue
@@ -19,39 +19,62 @@
                 </b-table-column>
 
                 <b-table-column field="email" label="Email">
-                    {{ props.row.email }}
+                    <b-tooltip label="Copy Email" type="is-dark">
+                        <vh-copy class="text-copyable"
+                                 :data="props.row.email"
+                                 :label="props.row.email"
+                                 @copied="copiedData"
+                        >
+                        </vh-copy>
+                    </b-tooltip>
+                </b-table-column>
+
+                <b-table-column v-if="props.row.deleted_at" field="is_active" label="Is Active">
+
+                    <b-button v-if="props.row.is_active === 1" rounded size="is-small"
+                              type="is-success">
+                        Yes
+                    </b-button>
+                    <b-button v-else rounded size="is-small" type="is-danger">
+                        No
+                    </b-button>
+
+                </b-table-column>
+
+                <b-table-column v-else field="is_active" label="Is Active">
+                    <b-tooltip label="Change Status" type="is-dark">
+                        <b-button v-if="props.row.is_active === 1" rounded size="is-small"
+                                  type="is-success" @click="changeStatus(props.row.id)">
+                            Yes
+                        </b-button>
+                        <b-button v-else rounded size="is-small" type="is-danger"
+                                  @click="changeStatus(props.row.id)">
+                            No
+                        </b-button>
+                    </b-tooltip>
+                </b-table-column>
+
+                <b-table-column field="roles" label="Roles">
+                    <b-tooltip label="View Role" type="is-dark">
+                        <b-button rounded size="is-small"
+                                  type="is-primary" @click="getRole(props.row)">
+                            {{ props.row.active_roles_count }} / {{page.total_roles}}
+                        </b-button>
+                    </b-tooltip>
                 </b-table-column>
 
                 <b-table-column field="status" label="Status">
-                    <span class="tag">
+                    <b-tag type="is-small">
                         {{ props.row.status }}
-                    </span>
+                    </b-tag>
                 </b-table-column>
 
-                <b-table-column field="updated_at" label="Updated At" >
-                    {{ $vaah.fromNow(props.row.updated_at) }}
+                <b-table-column field="last_login_at" label="Last Login At" >
+                    {{ $vaah.fromNow(props.row.last_login_at) }}
                 </b-table-column>
 
-                <b-table-column field="gender" label="Gender">
-                    <span>
-
-                        <b-icon pack="fas"
-                                v-if="props.row.gender === 'm'"
-                                icon="mars">
-                        </b-icon>
-
-                        <b-icon pack="fas"
-                                v-if="props.row.gender === 'f'"
-                                icon="venus">
-                        </b-icon>
-
-                        <b-icon pack="fas"
-                                v-if="props.row.gender === 'o'"
-                                icon="transgender">
-                        </b-icon>
-
-                        {{ props.row.gender }}
-                    </span>
+                <b-table-column field="created_at" label="Created At" >
+                    {{ $vaah.fromNow(props.row.created_at) }}
                 </b-table-column>
 
 
