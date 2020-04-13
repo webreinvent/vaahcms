3556aeb14ed0bae5229bbdd83af30146a98d0c1e
Pradeep <webreinvent@gmail.com>
2020-04-13 14:22:15 +0530

    few fixes

--- src/Resources/views/backend/vaahone/vue/app/store/store.js
+++ src/Resources/views/backend/vaahone/vue/app/store/store.js
@@ -6,11 +6,7 @@ Vue.use(Vuex);
 
 import root from './modules/root';
 import registrations from './modules/registrations';
-import users from './modules/users';
-import permissions from './modules/permissions';
-import roles from './modules/roles';
 import setup from './modules/setup';
-import permission from './modules/permission';
 import users from './modules/users';
 import permissions from './modules/permissions';
 import roles from './modules/roles';
@@ -19,11 +15,7 @@ export const store = new Vuex.Store({
     modules: {
         root: root,
         registrations: registrations,
-        permissions: permissions,
-        roles: roles,
-        users: users,
         setup: setup,
-        permission: permission,
         permissions: permissions,
         roles: roles,
         users: users,
