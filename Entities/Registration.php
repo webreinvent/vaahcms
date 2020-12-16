dfffd62fe64202e227d6ddc3846abd6ace082393
Pradeep <webreinvent@gmail.com>
2020-12-16 19:39:10 +0530

    Added: User & Registration Meta value in json format

--- Entities/Registration.php
+++ Entities/Registration.php
@@ -84,6 +84,16 @@ class Registration extends Model
         $this->attributes['password'] = \Hash::make($value);
     }
     //-------------------------------------------------
+    public function setMetaAttribute($value)
+    {
+        $this->attributes['meta'] = json_encode($value);
+    }
+    //-------------------------------------------------
+    public function getMetaAttribute($value)
+    {
+        return json_decode($value);
+    }
+    //-------------------------------------------------
     public function getNameAttribute() {
         return $this->first_name." ".$this->last_name;
     }
