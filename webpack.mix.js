df2e3c507a4b0602188fc367b9174b1a610cbc16
Pradeep <webreinvent@gmail.com>
2020-12-26 12:57:02 +0530

    backup

--- webpack.mix.js
+++ webpack.mix.js
@@ -52,8 +52,8 @@ if (mix.inProduction()) {
     //mix.sass(source_theme_folder+'/scss/backend.scss', output_theme_folder+'css/');
 
     //mix.js(__dirname+"/VueUI/app.js",  output_theme_folder+'/builds/ui.js');
-    //mix.js(__dirname+"/Vue/app.js",  output_theme_folder+'/builds/app.js');
-    mix.js(__dirname+"/Vue/app-extended.js",  output_theme_folder+'/builds/app-extended.js');
+    mix.js(__dirname+"/Vue/app.js",  output_theme_folder+'/builds/app.js');
+    //mix.js(__dirname+"/Vue/app-extended.js",  output_theme_folder+'/builds/app-extended.js');
 
 }
 
