b0dde02c54c09255598511eede17d75eacb296f3
vikram chand <vikram.wri231@webreinvent.com>
2021-01-19 11:08:06 +0530

    Fixed: search and change path of batch controller

--- Routes/backend/advanced.php
+++ Routes/backend/advanced.php
@@ -18,3 +18,36 @@ Route::group(
 
     });
 
+
+
+Route::group(
+    [
+        'prefix' => 'backend/batches',
+        'middleware' => ['web', 'has.backend.access'],
+        'namespace' => 'WebReinvent\VaahCms\Http\Controllers\Advanced',
+    ],
+    function () {
+        //---------------------------------------------------------
+        Route::get('/', 'BatchesController@getIndex')
+            ->name('vh.backend.job_batches.batches');
+        //---------------------------------------------------------
+        Route::any('/assets', 'BatchesController@getAssets')
+            ->name('vh.backend.job_batches.batches.assets');
+        //---------------------------------------------------------
+        Route::post('/create', 'BatchesController@postCreate')
+            ->name('vh.backend.job_batches.batches.create');
+        //---------------------------------------------------------
+        Route::any('/list', 'BatchesController@getList')
+            ->name('vh.backend.job_batches.batches.list');
+        //---------------------------------------------------------
+        Route::any('/item/{uuid}', 'BatchesController@getItem')
+            ->name('vh.backend.job_batches.batches.item');
+        //---------------------------------------------------------
+        Route::post('/store/{uuid}', 'BatchesController@postStore')
+            ->name('vh.backend.job_batches.batches.store');
+        //---------------------------------------------------------
+        Route::post('/actions/{action_name}', 'BatchesController@postActions')
+            ->name('vh.backend.job_batches.batches.actions');
+        //---------------------------------------------------------
+    });
+
