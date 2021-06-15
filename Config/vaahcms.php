a3c4e23b98c242c1b28b4432654cd50b66e44ffd
Pradeep <webreinvent@gmail.com>
2021-06-15 12:18:43 +0530

    version updated

--- Config/vaahcms.php
+++ Config/vaahcms.php
@@ -7,7 +7,7 @@
 $settings =  [
     'app_name' => 'VaahCMS',
     'app_slug' => 'vaahcms',
-    'version' => '1.2.3',
+    'version' => '1.3.0',
     'get_config_version' => false,
     'website' => 'https://vaah.dev/cms',
     'documentation' => 'https://vaah.dev/cms/docs',
