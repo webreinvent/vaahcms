9664ef3732f331e46c3e1b0675137c3c6302e7ad
vikram chand <vikram.wri231@webreinvent.com>
2021-09-28 16:19:51 +0530

    Updated: dashboard and interval issue in advanced section

--- Vue/pages/dashboard/Index.vue
+++ Vue/pages/dashboard/Index.vue
@@ -10,9 +10,53 @@
 
             <template v-for="list in root.assets.dashboard.success">
 
-                <div v-for="(item,key) in list" class="column is-3">
+                <div v-for="(item,key) in list" class="column is-one-fifth" style="display: grid">
 
-                    <div class="card" :class="item.link?'is-clickable':''"
+                    <div class="card" style="border-radius: 0.75rem"
+                         :class="item.link?'is-clickable ':''"
+                         @click="goToLink(item.link)">
+
+                        <header v-if="item.icon"
+                                class="card-header has-text-centered is-block">
+                            <b-icon :icon="item.icon"
+                                    size="is-large">
+                            </b-icon>
+                        </header>
+
+                        <div class="card-content">
+
+
+                            <div class="content has-text-centered">
+                                <span v-if="item.image" class="image is-96x96 is-inline-flex">
+                                    <img class="is-rounded" :src="item.image">
+                                </span>
+                                <span v-if="item.count" class="is-size-2 has-text-weight-bold">
+                                   {{item.count}}
+                                </span>
+                                <hr v-if="item.label"  />
+                                <span v-if="item.label" class="has-text-weight-bold">
+                                   {{item.label}}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                   <!-- <div class="card" :class="item.card_classes">
+                        <header class="card-header">
+                            <p class="card-header-title has-text-centered is-block">
+                                {{ $vaah.toLabel(key).toUpperCase()}}
+                            </p>
+                        </header>
+
+                        <div class="card-content">
+
+                            <div class="content is-size-1 has-text-centered has-text-weight-bold">
+                                {{ typeof item.count !== 'boolean'?item.count:' '}}
+                            </div>
+                        </div>
+                    </div>-->
+
+                   <!-- <div class="card" :class="item.link?'is-clickable':''"
                          @click="goToLink(item.link)">
                         <div class="card-content" :class="item.card_classes">
                             <div class="media is-vcentered">
@@ -33,7 +77,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div>-->
 
                 </div>
 
