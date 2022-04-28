b6631409d90d2ee57346a536a7c354a244420107
vikram chand <vikram.wri231@webreinvent.com>
2022-04-28 13:21:17 +0530

    Added: migration for vh_settings

---
+++ Database/Migrations/2022_04_28_130328_vh_settings_add_columns.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class VhSettingsAddColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('vh_settings', function (Blueprint $table) {
+            $table->bigInteger('created_by')->after('meta')
+                ->unsigned()->nullable()->index();
+            $table->foreign('created_by')
+                ->references('id')->on('vh_users');
+
+            $table->bigInteger('updated_by')->after('created_by')
+                ->unsigned()->nullable()->index();
+            $table->foreign('updated_by')
+                ->references('id')->on('vh_users');
+
+            $table->bigInteger('deleted_by')->after('updated_by')
+                ->unsigned()->nullable()->index();
+            $table->foreign('deleted_by')
+                ->references('id')->on('vh_users');
+
+            $table->softDeletes()->after('updated_at');
+            $table->index(['deleted_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vh_settings', function (Blueprint $table) {
+            $table->dropColumn('created_by');
+            $table->dropColumn('updated_by');
+            $table->dropColumn('deleted_by');
+            $table->dropSoftDeletes();
+        });
+    }
+}
