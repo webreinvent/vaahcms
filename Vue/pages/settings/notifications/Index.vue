9af305f43146a82e52f3e796638adc1d8fa84b2d
Jayshree Mahajan <jayshree.m001@webreinvent.com>
2022-07-02 17:55:36 +0530

    Added: Copy Slug button in Notifications

--- Vue/pages/settings/notifications/Index.vue
+++ Vue/pages/settings/notifications/Index.vue
@@ -121,7 +121,19 @@
 
                                     <div v-if="page.active_item">
 
-                                        <h3 class="title is-4">{{page.active_item.name}}</h3>
+                                        <h3 class="title is-4">
+                                            {{page.active_item.name}}
+                                            <b-tooltip label="Copy" type="is-dark">
+                                                <span   @click="$vaah.copy(page.active_item.slug)">
+                                                      <b-icon class="icon"
+                                                        icon="copy">
+                                                      </b-icon>
+                                                </span>
+                                            </b-tooltip>
+                                        </h3>
+
+
+
 
 
 
