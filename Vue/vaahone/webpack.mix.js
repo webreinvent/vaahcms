e2c6e4d79cf5e24385e307a26872636020d74ab6
Pradeep <webreinvent@gmail.com>
2022-10-08 16:12:50 +0530

    fixed: vaahone vue app

--- Vue/vaahone/webpack.mix.js
+++ Vue/vaahone/webpack.mix.js
@@ -4,7 +4,8 @@ const fs_extra = require('fs-extra');
 let Visualizer = require('webpack-visualizer-plugin');
 
 
-let theme_path = './../../Resources/assets/backend/vaahone/';
+let theme_name = 'vaahone';
+let theme_path = './../../Resources/assets/backend/'+theme_name+'/';
 let publish_folder;
 let output_theme_folder;
 let source_theme_folder;
@@ -57,21 +58,24 @@ if (mix.inProduction()) {
 
 } else {
 
-    /*publish_folder = './../../../../public/vaahcms/backend/';
+    /*
+    publish_folder = './../../../../public/vaahcms/backend/';
     output_theme_folder = "./"+theme_name+"/";
     source_theme_folder = "./../../Resources/assets/backend/"+theme_name;
-    source_vue_folder = __dirname+'/../../Resources/views/backend/'+theme_name+'/vue';*/
+    source_vue_folder = __dirname+'/../../Resources/views/backend/'+theme_name+'/vue';
+    //mix.js(__dirname+"/VueUI/app.js",  output_theme_folder+'/builds/ui.js').vue();
+    //mix.js(__dirname+"/Vue/app.js",  output_theme_folder+'/builds/app.js').sourceMaps();
 
+    */
+
+    publish_folder = './../../../../public/vaahcms/backend/'
     mix.setPublicPath(publish_folder);
 
-    mix.sass(source_theme_folder+'/scss/build.scss', output_theme_folder+'css/');
-    mix.sass(source_theme_folder+'/scss/style.scss', output_theme_folder+'css/');
+    mix.sass(theme_path+'/scss/build.scss', theme_name+'/css/');
+    mix.sass(theme_path+'/scss/style.scss', theme_name+'/css/');
 
-    //mix.js(__dirname+"/VueUI/app.js",  output_theme_folder+'/builds/ui.js').vue();
-
-    mix.js(__dirname+"/app.js",  output_theme_folder+'/builds/app.js').vue();
-    //mix.js(__dirname+"/Vue/app.js",  output_theme_folder+'/builds/app.js').sourceMaps();
-    mix.js(__dirname+"/app-extended.js",  output_theme_folder+'/builds/app-extended.js').vue();
+    mix.js(__dirname+"/app.js",  theme_name+'/builds/app.js').vue();
+    mix.js(__dirname+"/app-extended.js",  theme_name+'/builds/app-extended.js').vue();
 
 }
 
