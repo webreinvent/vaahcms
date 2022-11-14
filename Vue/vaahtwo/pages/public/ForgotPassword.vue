d9d0901a1c5ad5daf0ba0de265aae290c6fab4cc
Haresh <haresh.p001@webreinvent.com>
2022-11-14 18:21:23 +0530

    Updated: Forgot Password page updated

---
+++ Vue/vaahtwo/pages/public/ForgotPassword.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+import {onMounted, reactive} from "vue";
+
+
+import { useRootStore } from '../../stores/root';
+const root = useRootStore();
+
+
+onMounted(async () => {
+
+});
+
+</script>
+<template>
+    <Card style="width: 28rem;max-width: 100vw; margin-bottom: 2em" class="m-auto">
+        <template #title>
+            <div class="content text-center">
+                <img src="http://irisrishu.com/vaahcms/backend/vaahone/images/vaahcms-logo.svg" alt="" class="w-5 mb-2">
+                <h4 class="text-xl font-semibold line-height-2 mb-2">Forgot password?</h4>
+                <p class="text-sm text-gray-600 font-semibold mb-5">You can recover your password from here.</p>
+            </div>
+        </template>
+        <template #content>
+            <div>
+                <form>
+                    <div class="p-float-label field mb-5">
+                        <InputText
+                            v-model="root.forgot_password_items.email"
+                            name="forgot_password-email"
+                            data-testid="forgot_password-email"
+                            id="email"
+                            class="w-full"
+                            type="text"/>
+                        <label for="email">Enter Email Address</label>
+                    </div>
+                    <div class="field flex justify-content-between align-items-center">
+                        <Button
+                            label="Send Code"
+                            class="p-button-sm"
+                            native-type="submit"
+                            @click="root.onSendCode()"
+                            :loading="root.is_forgot_password_btn_loading"/>
+                        <router-link :to="{name:'sign.in'}">
+                            <Button label="Sign In" class="p-button-text p-button-sm"/>
+                        </router-link>
+                    </div>
+                </form>
+            </div>
+        </template>
+        <template #footer>
+            <div class="text-xs text-center"><p>
+                © 2022.
+                <a class="text-blue-400" href="https://vaah.dev/cms" target="_blank">VaahCMS</a>
+                v1.6.10
+                | <a class="text-blue-400" href="https://docs.vaah.dev/vaahcms" target="_blank">Documentation</a></p> <p class="has-text-centered">
+                Laravel v8.41.0
+                | PHP v8.0.18
+            </p></div>
+        </template>
+    </Card>
+</template>
+
