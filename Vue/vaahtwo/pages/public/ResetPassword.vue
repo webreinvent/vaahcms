bc180d22fa2b0bceb4a6ca39f304368e17a651dd
Haresh <haresh.p001@webreinvent.com>
2022-11-15 15:37:39 +0530

    Updated: Create auth file in forgot or reset password file

--- Vue/vaahtwo/pages/public/ResetPassword.vue
+++ Vue/vaahtwo/pages/public/ResetPassword.vue
@@ -19,8 +19,8 @@ onMounted(async () => {
 <template>
     <Card style="width: 28rem;max-width: 100vw; margin-bottom: 2em" class="m-auto">
         <template #title>
-            <div class="content text-center" v-if="root && root.assets">
-                <img :src="root.assets.backend_logo_url" alt="" class="w-5 mb-2">
+            <div class="content text-center" v-if="auth && auth.assets">
+                <img :src="auth.assets.backend_logo_url" alt="" class="w-5 mb-2">
                 <h4 class="text-xl font-semibold line-height-2 mb-2">Reset password?</h4>
                 <p class="text-sm text-gray-600 font-semibold mb-5">You can recover your password from here.</p>
             </div>
