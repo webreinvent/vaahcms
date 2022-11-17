404a44231a02b7717b76ed4913f49b934db657af
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2022-11-17 20:18:20 +0530

    Updated: dynamic side menu (wip)

--- Vue/vaahtwo/layouts/Backend.vue
+++ Vue/vaahtwo/layouts/Backend.vue
@@ -6,6 +6,7 @@ import Aside from '../components/molecules/Aside.vue';
 import { useRootStore } from '../stores/root'
 import Topnav from "../components/organisms/Topnav.vue";
 import Footer from "../components/organisms/Footer.vue";
+import Sidebar from "../components/molecules/Sidebar.vue";
 
 const rootStore = useRootStore();
 
@@ -22,7 +23,8 @@ onMounted(async () => {
 
     <div class="grid">
         <div><Topnav /></div>
-        <div class="col-2 mt-6"><Aside/></div>
+        <div class="col-2 mt-6"> <Sidebar/> </div>
+<!--        <div class="col-2 mt-6"><Aside/></div>-->
         <div class="col mt-6"><RouterView /></div>
     </div>
 
