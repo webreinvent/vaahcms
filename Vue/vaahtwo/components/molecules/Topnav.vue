db586af2fba368f7bd93d0f65fe51380f6032511
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2022-11-19 20:36:21 +0530

    Updated: dynamic topnav (wip)

---
+++ Vue/vaahtwo/components/molecules/Topnav.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { onMounted , ref} from 'vue';
+import { useRootStore } from "../../stores/root";
+
+const root = useRootStore();
+
+onMounted(async () => {
+    root.verifyInstallStatus();
+    await root.getAssets();
+});
+
+const menu = ref();
+
+const toggle = (event) => {
+    menu.value.toggle(event);
+}
+
+const items = ref( [
+    {
+        to:'/',
+        title:'',
+        icon:'pi pi-home'
+    },
+    {
+        to:'/',
+        title:'',
+        icon:'pi pi-link'
+    }
+]);
+
+const menu_options = ref([
+    {
+        label:'Profile',
+        icon:'pi pi-fw pi-user',
+        command: () =>{
+        }
+    },
+    {
+        label:'Logout',
+        icon:'pi pi-fw pi-sign-out',
+        command: () =>{
+        }
+    }
+]);
+
+const menu_item = ref();
+
+if(root &&  root.assets && root.assets.extended_views) {
+    root.assets.extended_views.top_right_user_menu.success.forEach(item);
+}
+
+</script>
+
+<template>
+    <div v-if="root && root.assets && root.assets.extended_views && root.assets.extended_views.top_right_user_menu">
+        <Menubar :model="items" class="top-nav-fixed py-2">
+            <template #start>
+                <div class="navbar-logo">
+                    <img src="https://develop.jalapeno.app/vaahcms/backend/vaahone/images/vaahcms-logo.svg" alt="VaahCMS">
+                </div>
+            </template>
+
+            <template #item="{item}">
+                <router-link :to="item.to" custom v-slot="{href, route, navigate, isActive, isExactActive}">
+                    <a :href="href" @click="navigate" class="mx-2"><i class="pi" :class="item.icon"></i></a>
+                </router-link>
+            </template>
+
+            <template #end>
+                <Button icon="pi pi-bars" @click="toggle"
+                        aria-haspopup="true"
+                        aria-controls="overlay_menu"
+                />
+
+                <Button icon="pi pi-bars" @click="log" />
+
+                <TieredMenu id="overlay_menu" :model="menu_item" ref="menu" :popup="true" />
+            </template>
+        </Menubar>
+    </div>
+</template>
