2f5ad7ab8a6a5c2ebd01978585ce8309c9cb0d1e
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2022-11-28 15:25:29 +0530

    Added: permission role page

---
+++ Vue/vaahtwo/pages/permissions/ViewRole.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import {onMounted} from "vue";
+
+onMounted( async () => {
+    /**
+     * fetch assets required for the crud
+     * operation
+     */
+    await store.getAssets();
+});
+
+</script>
+
+<template>
+    <div>
+        <Panel v-if="store && store.item"></Panel>
+    </div>
+</template>
