157ab84d05df519a5a8958211dd92e4942225cec
we-abinash-s001 <abinash.s001@webreinvent.com>
2023-01-05 20:07:35 +0530

    Update: Job module from vue2 to vue3

---
+++ Vue/vaahtwo/pages/jobs/List.vue
@@ -0,0 +1,93 @@
+<script setup>
+import {onMounted, reactive, ref} from "vue";
+import {useRoute} from 'vue-router';
+
+import {useJobStore} from '../../stores/store-jobs'
+
+import Actions from "./components/Actions.vue";
+import Table from "./components/Table.vue";
+
+const store = useJobStore();
+const route = useRoute();
+
+import { useConfirm } from "primevue/useconfirm";
+const confirm = useConfirm();
+
+
+onMounted(async () => {
+    /**
+     * call onLoad action when List view loads
+     */
+    await store.onLoad(route);
+
+    /**
+     * watch routes to update view, column width
+     * and get new item when routes get changed
+     */
+    await store.watchRoutes(route);
+
+    /**
+     * watch states like `query.filter` to
+     * call specific actions if a state gets
+     * changed
+     */
+    await store.watchStates();
+
+    /**
+     * fetch assets required for the crud
+     * operation
+     */
+    await store.getAssets();
+
+    /**
+     * fetch list of records
+     */
+    await store.getList();
+});
+
+</script>
+<template>
+
+    <div class="grid" v-if="store.assets">
+
+        <div :class="'col-'+store.list_view_width">
+            <Panel>
+
+                <template class="p-1" #header>
+
+                    <div class="flex flex-row">
+                        <div >
+                            <b class="mr-1">Jobs</b>
+                            <Badge v-if="store.list && store.list.total > 0"
+                                   :value="store.list.total">
+                            </Badge>
+                        </div>
+
+                    </div>
+
+                </template>
+                <template class="p-1">
+                    <b-notification type="is-info"
+                                    :closable="false"
+                                    class="is-light is-small">
+                        This list consist of only queued/pending jobs.
+                        Completed jobs gets deleted automatically .
+                    </b-notification>
+                </template>
+
+
+                <Actions/>
+
+                <br/>
+
+                <Table/>
+
+            </Panel>
+        </div>
+
+        <RouterView/>
+
+    </div>
+
+
+</template>
---
+++ Vue/vaahtwo/pages/jobs/components/Filters.vue
@@ -0,0 +1,110 @@
+<script  setup>
+
+import { useJobStore } from '../../../stores/store-jobs'
+import VhViewRow from '../../../vaahvue/vue-three/primeflex/VhViewRow.vue';
+
+
+const store = useJobStore();
+
+</script>
+
+<template>
+    <div>
+
+        <Sidebar v-model:visible="store.show_filters"
+                 position="right">
+
+            <VhFieldVertical >
+                <template #label>
+                    <b>Sort By:</b>
+                </template>
+
+                <div class="field-radiobutton">
+                    <RadioButton name="sort-none"
+                                 data-testid="jobs-filters-sort-none"
+                                 value=""
+                                 v-model="store.query.filter.sort" />
+                    <label for="sort-none">None</label>
+                </div>
+                <div class="field-radiobutton">
+                    <RadioButton name="sort-ascending"
+                                 data-testid="jobs-filters-sort-ascending"
+                                 value="updated_at"
+                                 v-model="store.query.filter.sort" />
+                    <label for="sort-ascending">Updated (Ascending)</label>
+                </div>
+                <div class="field-radiobutton">
+                    <RadioButton name="sort-descending"
+                                 data-testid="jobs-filters-sort-descending"
+                                 value="updated_at:desc"
+                                 v-model="store.query.filter.sort" />
+                    <label for="sort-descending">Updated (Descending)</label>
+                </div>
+
+            </VhFieldVertical>
+
+            <Divider/>
+
+            <VhFieldVertical >
+                <template #label>
+                    <b>Is Active:</b>
+                </template>
+
+                <div class="field-radiobutton">
+                    <RadioButton name="active-all"
+                                 value="null"
+                                 data-testid="jobs-filters-active-all"
+                                 v-model="store.query.filter.is_active" />
+                    <label for="active-all">All</label>
+                </div>
+                <div class="field-radiobutton">
+                    <RadioButton name="active-true"
+                                 data-testid="jobs-filters-active-true"
+                                 value="true"
+                                 v-model="store.query.filter.is_active" />
+                    <label for="active-true">Only Active</label>
+                </div>
+                <div class="field-radiobutton">
+                    <RadioButton name="active-false"
+                                 data-testid="jobs-filters-active-false"
+                                 value="false"
+                                 v-model="store.query.filter.is_active" />
+                    <label for="active-false">Only Inactive</label>
+                </div>
+
+            </VhFieldVertical>
+
+            <VhFieldVertical >
+                <template #label>
+                    <b>Trashed:</b>
+                </template>
+
+                <div class="field-radiobutton">
+                    <RadioButton name="trashed-exclude"
+                                 data-testid="jobs-filters-trashed-exclude"
+                                 value=""
+                                 v-model="store.query.filter.trashed" />
+                    <label for="trashed-exclude">Exclude Trashed</label>
+                </div>
+                <div class="field-radiobutton">
+                    <RadioButton name="trashed-include"
+                                 data-testid="jobs-filters-trashed-include"
+                                 value="include"
+                                 v-model="store.query.filter.trashed" />
+                    <label for="trashed-include">Include Trashed</label>
+                </div>
+                <div class="field-radiobutton">
+                    <RadioButton name="trashed-only"
+                                 data-testid="jobs-filters-trashed-only"
+                                 value="only"
+                                 v-model="store.query.filter.trashed" />
+                    <label for="trashed-only">Only Trashed</label>
+                </div>
+
+            </VhFieldVertical>
+
+
+        </Sidebar>
+
+    </div>
+</template>
