d4d4a77de0c13b6f03fcd23439a07b316f774f76
Peeyush Sengar <peeyush.s001@wereinvent.com>
2023-01-09 20:36:15 +0530

    Added: Taxonomy crud added

---
+++ Http/Controllers/Backend/TaxonomiesController.php
@@ -0,0 +1,102 @@
+<?php namespace WebReinvent\\VaahCms\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use WebReinvent\\VaahCms\Models\Taxonomy;
+
+
+class TaxonomiesController extends Controller
+{
+
+
+    //----------------------------------------------------------
+    public function __construct()
+    {
+
+    }
+
+    //----------------------------------------------------------
+
+    public function getAssets(Request $request)
+    {
+
+        $data = [];
+
+        $data['permission'] = [];
+        $data['rows'] = config('vaahcms.per_page');
+
+        $data['fillable']['except'] = [
+            'uuid',
+            'created_by',
+            'updated_by',
+            'deleted_by',
+        ];
+
+        $model = new Taxonomy();
+        $fillable = $model->getFillable();
+        $data['fillable']['columns'] = array_diff(
+            $fillable, $data['fillable']['except']
+        );
+
+        foreach ($fillable as $column)
+        {
+            $data['empty_item'][$column] = null;
+        }
+
+        $data['actions'] = [];
+
+        $response['success'] = true;
+        $response['data'] = $data;
+
+        return $response;
+    }
+
+    //----------------------------------------------------------
+    public function getList(Request $request)
+    {
+        return Taxonomy::getList($request);
+    }
+    //----------------------------------------------------------
+    public function updateList(Request $request)
+    {
+        return Taxonomy::updateList($request);
+    }
+    //----------------------------------------------------------
+    public function listAction(Request $request, $type)
+    {
+        return Taxonomy::listAction($request, $type);
+    }
+    //----------------------------------------------------------
+    public function deleteList(Request $request)
+    {
+        return Taxonomy::deleteList($request);
+    }
+    //----------------------------------------------------------
+    public function createItem(Request $request)
+    {
+        return Taxonomy::createItem($request);
+    }
+    //----------------------------------------------------------
+    public function getItem(Request $request, $id)
+    {
+        return Taxonomy::getItem($id);
+    }
+    //----------------------------------------------------------
+    public function updateItem(Request $request,$id)
+    {
+        return Taxonomy::updateItem($request,$id);
+    }
+    //----------------------------------------------------------
+    public function deleteItem(Request $request,$id)
+    {
+        return Taxonomy::deleteItem($request,$id);
+    }
+    //----------------------------------------------------------
+    public function itemAction(Request $request,$id,$action)
+    {
+        return Taxonomy::itemAction($request,$id,$action);
+    }
+    //----------------------------------------------------------
+
+
+}
