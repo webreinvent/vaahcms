8255cb8e814e41e57dbca021468a1d4f51478a46
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2023-01-10 14:06:04 +0530

    Updated: table UI

--- Vue/vaahtwo/pages/registrations/components/Table.vue
+++ Vue/vaahtwo/pages/registrations/components/Table.vue
@@ -8,7 +8,6 @@ const useVaah = vaah();
 </script>
 
 <template>
-
     <div v-if="store.list">
         <!--table-->
          <DataTable :value="store.list.data"
@@ -20,50 +19,50 @@ const useVaah = vaah();
 
             <Column selectionMode="multiple"
                     v-if="store.isViewLarge()"
-                    headerStyle="width: 3em">
-            </Column>
+                    headerStyle="width: 3em"
+            />
 
-            <Column field="id" header="ID" :style="{width: store.getIdWidth()}" :sortable="true">
-            </Column>
+            <Column field="id" header="ID" :style="{width: store.getIdWidth()}" :sortable="true" />
 
              <Column field="Name"
                     header="Name"
-                    :sortable="true">
+                    :sortable="true"
+             >
                 <template #body="prop">
                     <Badge v-if="prop.data.deleted_at"
                            value="Trashed"
-                           severity="danger"></Badge>
+                           severity="danger"
+                    />
                     {{prop.data.name}}
                 </template>
             </Column>
+
              <Column field="email"
                     header="Email"
-                    :sortable="true">
+                    :sortable="true"
+             >
                 <template #body="prop">
-                    <Badge v-if="prop.data.deleted_at"
-                           value="Trashed"
-                           severity="danger"></Badge>
                     {{prop.data.email}}
                 </template>
             </Column>
 
              <Column field="status"
-                    header="Status"
-                    :sortable="true">
+                     header="Status"
+             >
                 <template #body="prop">
-                    <Badge v-if="prop.data.deleted_at"
-                           value="Trashed"
-                           severity="danger"></Badge>
-                    <Tag class="mr-2" severity="success"  v-if="prop.data.status">{{prop.data.status}}</Tag>
+                    <Tag severity="success"
+                         v-if="prop.data.status"
+                    >
+                        {{prop.data.status}}
+                    </Tag>
                 </template>
             </Column>
-
-
 
              <Column field="updated_at" header="Updated"
                      v-if="store.isViewLarge()"
                      style="width:150px;"
-                     :sortable="true">
+                     :sortable="true"
+             >
                  <template #body="prop">
                      {{useVaah.ago(prop.data.updated_at)}}
                  </template>
@@ -72,24 +71,38 @@ const useVaah = vaah();
              <Column field="gender"
                     header="Gender"
                     v-if="store.isViewLarge()"
-                    :sortable="true">
-                <template #body="prop"  >
-                    <Badge v-if="prop.data.deleted_at"
-                           value="Trashed"
-                           severity="danger"></Badge>
-                     <Tag severity="primary" class="mr-2" v-if="prop.data.gender && prop.data.gender=='m'">Male</Tag>
-                     <Tag severity="primary" class="mr-2" v-if="prop.data.gender && prop.data.gender=='f'">Female</Tag>
-                     <Tag severity="primary" class="mr-2" v-if="prop.data.gender && prop.data.gender=='o'">Other</Tag>
+                    :sortable="true"
+             >
+                <template #body="prop">
+                     <Tag severity="primary"
+                          class="mr-2"
+                          v-if="prop.data.gender && prop.data.gender === 'm'"
+                     >
+                         Male
+                     </Tag>
+
+                     <Tag severity="primary"
+                          class="mr-2"
+                          v-if="prop.data.gender && prop.data.gender === 'f'"
+                     >
+                         Female
+                     </Tag>
+
+                     <Tag severity="primary"
+                          class="mr-2"
+                          v-if="prop.data.gender && prop.data.gender === 'o'"
+                     >
+                         Other
+                     </Tag>
                 </template>
             </Column>
-
 
             <Column field="actions" style="width:150px;"
                     :style="{width: store.getActionWidth() }"
                     :header="store.getActionLabel()">
 
                 <template #body="prop">
-                    <div class="p-inputgroup ">
+                    <div class="p-inputgroup">
 
                         <Button class="p-button-tiny p-button-text"
                                 v-tooltip.top="'View'"
@@ -113,7 +126,6 @@ const useVaah = vaah();
                                 data-testid="register-table_item_action_restore"
                         />
 
-
                         <Button class="p-button-tiny p-button-success p-button-text"
                                 v-if="store.isViewLarge() && prop.data.deleted_at"
                                 @click="store.itemAction('restore', prop.data)"
@@ -121,16 +133,9 @@ const useVaah = vaah();
                                 icon="pi pi-replay"
                                 data-testid="register-table_item_action_delete"
                         />
-
-
                     </div>
-
                 </template>
-
-
             </Column>
-
-
         </DataTable>
         <!--/table-->
 
@@ -142,10 +147,7 @@ const useVaah = vaah();
                    @page="store.paginate($event)"
                    :rowsPerPageOptions="store.rows_per_page"
                    data-testid="register-table_paginate"
-        >
-        </Paginator>
+        />
         <!--/paginator-->
-
     </div>
-
 </template>
