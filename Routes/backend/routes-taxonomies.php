75ea53d48811f0932517188f0799651e2ca0c3da
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2023-01-21 13:08:01 +0530

    Added: taxonomy related crud

---
+++ Routes/backend/routes-taxonomies.php
@@ -0,0 +1,69 @@
+<?php
+
+Route::group(
+    [
+        'prefix' => 'backend/vaah/taxonomies',
+
+        'middleware' => ['web', 'has.backend.access'],
+
+        'namespace' => 'WebReinvent\VaahCms\Http\Controllers\Backend',
+],
+function () {
+    /**
+     * Get Assets
+     */
+    Route::get('/assets', 'TaxonomiesController@getAssets')
+        ->name('vh.backend.vaah.taxonomies.assets');
+    /**
+     * Get List
+     */
+    Route::get('/', 'TaxonomiesController@getList')
+        ->name('vh.backend.vaah.taxonomies.list');
+    /**
+     * Update List
+     */
+    Route::match(['put', 'patch'], '/', 'TaxonomiesController@updateList')
+        ->name('vh.backend.vaah.taxonomies.list.update');
+    /**
+     * Delete List
+     */
+    Route::delete('/', 'TaxonomiesController@deleteList')
+        ->name('vh.backend.vaah.taxonomies.list.delete');
+
+
+    /**
+     * Create Item
+     */
+    Route::post('/', 'TaxonomiesController@createItem')
+        ->name('vh.backend.vaah.taxonomies.create');
+    /**
+     * Get Item
+     */
+    Route::get('/{id}', 'TaxonomiesController@getItem')
+        ->name('vh.backend.vaah.taxonomies.read');
+    /**
+     * Update Item
+     */
+    Route::match(['put', 'patch'], '/{id}', 'TaxonomiesController@updateItem')
+        ->name('vh.backend.vaah.taxonomies.update');
+    /**
+     * Delete Item
+     */
+    Route::delete('/{id}', 'TaxonomiesController@deleteItem')
+        ->name('vh.backend.vaah.taxonomies.delete');
+
+    /**
+     * List Actions
+     */
+    Route::any('/action/{action}', 'TaxonomiesController@listAction')
+        ->name('vh.backend.vaah.taxonomies.list.actions');
+
+    /**
+     * Item actions
+     */
+    Route::any('/{id}/action/{action}', 'TaxonomiesController@itemAction')
+        ->name('vh.backend.vaah.taxonomies.item.action');
+
+    //---------------------------------------------------------
+
+});
--- Vue/vaahtwo/routes/routes.js
+++ Vue/vaahtwo/routes/routes.js
@@ -9,6 +9,7 @@ import advanced from "./vue-routes-advanced";
 import permissions from "./vue-routes-permissions";
 import settings from "./vue-routes-settings";
 import registrations from "./vue-routes-registrations";
+import taxonomies from "./vue-routes-taxonomies";
 
 routes = routes.concat(ui);
 routes = routes.concat(public_routes);
@@ -19,5 +20,6 @@ routes = routes.concat(users);
 routes = routes.concat(permissions);
 routes = routes.concat(settings);
 routes = routes.concat(registrations);
+routes = routes.concat(taxonomies);
 
 export default routes;
