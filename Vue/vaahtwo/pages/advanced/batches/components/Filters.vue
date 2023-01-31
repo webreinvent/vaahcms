77ede426b3b0a842ca0d14335369df79923fac0a
nitin kumar <nitin.k001@webinvent.com>
2023-01-31 20:17:51 +0530

    changes  made to date filter. Changes made to popup window for Details and Failed ids

--- Vue/vaahtwo/pages/advanced/batches/components/Filters.vue
+++ Vue/vaahtwo/pages/advanced/batches/components/Filters.vue
@@ -15,40 +15,7 @@ const dateColumns = [
 <template>
     <div>
 
-        <Sidebar v-model:visible="store.show_filters"
-                 position="right">
-
-            <VhFieldVertical >
-                <template #label>
-                    <b>Sort By:</b>
-                </template>
-
-                <div class="field-radiobutton">
-                    <RadioButton name="sort-none"
-                                 data-testid="batches-filters-sort-none"
-                                 value=""
-                                 v-model="store.query.filter.sort" />
-                    <label for="sort-none">None</label>
-                </div>
-                <div class="field-radiobutton">
-                    <RadioButton name="sort-ascending"
-                                 data-testid="batches-filters-sort-ascending"
-                                 value="updated_at"
-                                 v-model="store.query.filter.sort" />
-                    <label for="sort-ascending">Updated (Ascending)</label>
-                </div>
-                <div class="field-radiobutton">
-                    <RadioButton name="sort-descending"
-                                 data-testid="batches-filters-sort-descending"
-                                 value="updated_at:desc"
-                                 v-model="store.query.filter.sort" />
-                    <label for="sort-descending">Updated (Descending)</label>
-                </div>
-
-            </VhFieldVertical>
-
-            <Divider/>
-
+        <Sidebar v-model:visible="store.show_filters" position="right">
             <VhFieldVertical >
                 <template #label>
                     <b>Column:</b>
@@ -57,31 +24,30 @@ const dateColumns = [
                     <RadioButton name="sort-descending"
                                  data-testid="jobs-filters-sort-descending"
                                  value="created_at:desc"
-                                 v-model="store.query.filter.dateColumn" />
+                                 v-model="store.query.date_filter_by" />
                     <label for="sort-descending">Created (Descending)</label>
                 </div>
                 <div class="field-radiobutton">
                     <RadioButton name="sort-descending"
                                  data-testid="jobs-filters-sort-descending"
                                  value="cancelled_at:desc"
-                                 v-model="store.query.filter.dateColumn" />
+                                 v-model="store.query.date_filter_by" />
                     <label for="sort-descending">Cancelled (Descending)</label>
                 </div>
                 <div class="field-radiobutton">
                     <RadioButton name="sort-descending"
                                  data-testid="jobs-filters-sort-descending"
                                  value="finished_at:desc"
-                                 v-model="store.query.filter.dateColumn" />
+                                 v-model="store.query.date_filter_by" />
                     <label for="sort-descending">Finished (Descending)</label>
                 </div>
             </VhFieldVertical>
             <VhFieldVertical>
                 <Calendar inputId="range"
-                          v-model="store.query.filter.datesRange"
+                          v-model="store.dates2"
+                          @date-select="store.setDateRange"
                           selectionMode="range"
-                          :manualInput="false"
-                          :hideOnDateTimeSelect="true"
-                />
+                          :manualInput="false" />
             </VhFieldVertical>
         </Sidebar>
     </div>
