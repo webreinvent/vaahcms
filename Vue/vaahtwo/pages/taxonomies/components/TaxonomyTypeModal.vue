e6879c4682db90e9d0fe5436a9c56834fb0708a4
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2023-02-01 13:50:52 +0530

    Removed: commented codes and unnecessary files

--- Vue/vaahtwo/pages/taxonomies/components/TaxonomyTypeModal.vue
+++ Vue/vaahtwo/pages/taxonomies/components/TaxonomyTypeModal.vue
@@ -1,50 +1,19 @@
 <script setup>
 
-import {useTaxonomyStore} from "../../../stores/store-taxonomies";
-import NodeService from "../json/NodeService"
-import { onMounted, ref } from "vue";
+import { useTaxonomyStore } from "../../../stores/store-taxonomies";
 
 const store = useTaxonomyStore();
 
-const nodeService = ref(new NodeService());
-const nodes = ref();
-
-onMounted( async () => {
-    nodeService.value.getTreeNodes().then(data => nodes.value = data);
-});
 </script>
 
 <template>
     <div>
         <div class="p-inputgroup" v-if="store && store.assets">
-<!--            <Dropdown v-model="store.assets.type"-->
-<!--                      :options="store.assets.type"-->
-<!--                      optionLabel="name"-->
-<!--                      optionValue="code"-->
-<!--                      placeholder="Select a Parent"-->
-<!--            />-->
-
-<!--            <Dropdown v-model="store.item.type"-->
-<!--                      :options="store.assets.types"-->
-<!--                      optionLabel="name"-->
-<!--                      optionValue="id"-->
-<!--                      data-testid="taxonomy-type"-->
-<!--                      name="taxonomy-type"-->
-<!--                      placeholder="Select a parent"-->
-<!--            />-->
 
             <TreeSelect v-model="store.item.type"
-                        :options="nodes"
+                        :options="store.assets.types"
                         placeholder="Select a Parent"
             />
-
-<!--            <CascadeSelect :options="store.assets.type"-->
-<!--                           optionLabel="name"-->
-<!--                           optionGroupLabel="name"-->
-<!--                           :optionGroupChildren="['countries', 'cities']"-->
-<!--                           style="minWidth: 14rem"-->
-<!--                           placeholder="Select a Parent"-->
-<!--            />-->
 
             <InputText class="p-inputtext-sm"
                        name="taxonomies-slug"
@@ -60,6 +29,8 @@ onMounted( async () => {
 
         <Divider />
 
-        <Tree :value="nodes" />
+<!--        <Tree :value="nodes" />-->
+
+        <Tree :value="store.assets.types" selectionMode="single" />
     </div>
 </template>
--- Vue/vaahtwo/pages/taxonomies/json/NodeService.js
+++
@@ -1,8 +0,0 @@
-export default class NodeService {
-
-    getTreeNodes() {
-        return fetch('backend/vaah/manage/taxonomies/test.json').then(res => res.json())
-            .then(d => d.root);
-    }
-
-}
