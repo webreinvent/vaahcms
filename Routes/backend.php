194d7a9dddce6b0117c05e059145b0ef43fc4f23
vikram chand <vikram.wri231@webreinvent.com>
2023-02-03 12:55:46 +0530

    Fixed: conflict issue

--- Routes/backend.php
+++ Routes/backend.php
@@ -25,8 +25,6 @@ include('backend/advanced.php');
 include('backend/routes-roles.php');
 include('backend/routes-permissions.php');
 include('backend/routes-users.php');
-include('backend/routes-jobs.php');
-include('backend/routes-failedjobs.php');
 
 
 
