207978e79c284ee77c00fb42ad0aa3f2b9a04e65
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2023-02-15 16:37:11 +0530

    Updated: shift social media links to a separate component

--- Vue/vaahtwo/pages/settings/general/Index.vue
+++ Vue/vaahtwo/pages/settings/general/Index.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { onMounted } from "vue";
 import { useRoute } from 'vue-router';
+import { useGeneralStore } from "../../../stores/settings/store-general_setting";
 import draggable from 'vuedraggable';
 import { vaah } from '../../../vaahvue/pinia/vaah';
 import SiteSettings from './components/SiteSettings.vue';
 import DateTime from './components/DateTime.vue';
-
-import { useGeneralStore } from "../../../stores/settings/store-general_setting";
+import SocialMediaLink from './components/SocialMediaLink.vue'
 
 const store = useGeneralStore();
 const route = useRoute();
@@ -76,67 +76,7 @@ onMounted(async () => {
                             </div>
                         </template>
 
-                        <div class="grid">
-                            <div class="col-12 md:col-4" v-for="(item,index) in store.social_media_links">
-                                <h5 class="p-1 text-xs mb-1">{{ vaah().toLabel(item.label) }}</h5>
-                                <div class="p-inputgroup p-fluid">
-                                    <span class="p-input-icon-left">
-                                        <i :class="item.icon?'pi z-5 '+item.icon:'pi z-5 pi-link'"/>
-                                        <InputText type="text"
-                                                   :data-testid="'general-'+item.label+'field'"
-                                                   v-model="item.value"
-                                                   :placeholder="'Enter ' + item.label + ' Link'"
-                                                   class="w-full p-inputtext-sm"
-                                        />
-                                    </span>
-
-                                    <Button icon="pi pi-copy"
-                                            data-testid="general-link_copy"
-                                            :disabled="!item.id"
-                                            @click="store.getCopy(item.key)"
-                                            class=" p-button-sm"
-                                    />
-
-                                    <Button icon="pi pi-trash"
-                                            data-testid="general-link_remove"
-                                            @click="store.removeVariable(item)"
-                                            class="p-button-danger p-button-sm"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="grid mt-5">
-                            <div class="col-12 md:col-4">
-                                <div class="p-inputgroup">
-                                    <InputText v-model="store.add_link"
-                                               data-testid="general-add_link_field"
-                                               icon= "pi pi-link"
-                                               v-if="store.show_link_input"
-                                               class="p-inputtext-sm"
-                                    />
-
-                                    <Button label="Add Link"
-                                            icon="pi pi-plus"
-                                            class="p-button-sm"
-                                            data-testid="general-add_link_btn"
-                                            :disabled="!store.add_link"
-                                            @click="store.addLinkHandler"
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="col-12 md:col-8">
-                                <div class="p-inputgroup justify-content-end">
-                                    <Button label="Save"
-                                            icon="pi pi-save"
-                                            data-testid="general-link_save"
-                                            @click="store.storeLinks()"
-                                            class="p-button-sm"
-                                    />
-                                </div>
-                            </div>
-                        </div>
+                        <SocialMediaLink />
                     </AccordionTab>
 
                     <AccordionTab>
