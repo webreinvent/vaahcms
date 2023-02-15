1c0bc2062b6be874417c839fa33f77c6ea14a1f6
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2023-02-15 17:34:13 +0530

    Updated: shift meta tags to a separate component

--- Vue/vaahtwo/pages/settings/general/Index.vue
+++ Vue/vaahtwo/pages/settings/general/Index.vue
@@ -8,6 +8,7 @@ import SiteSettings from './components/SiteSettings.vue';
 import DateTime from './components/DateTime.vue';
 import SocialMediaLink from './components/SocialMediaLink.vue';
 import Script from './components/Scripts.vue';
+import MetaTags from './components/MetaTags.vue'
 
 const store = useGeneralStore();
 const route = useRoute();
@@ -99,59 +100,7 @@ onMounted(async () => {
                             </div>
                         </template>
 
-                        <div class="grid">
-                            <div class="col-12" v-if="store.meta_tag" v-for="(item,index) in store.meta_tag">
-                                <h5 class="p-1 text-xs mb-1">Meta Tag</h5>
-                                <div class="p-inputgroup">
-                                    <Dropdown v-model="item.value.attribute"
-                                              :options="store.assets.vh_meta_attributes"
-                                              optionLabel="name"
-                                              optionValue="slug"
-                                              data-testid="general-metatags_attributes"
-                                              placeholder="Select any"
-                                    />
-
-                                    <InputText v-model="item.value.attribute_value"
-                                               data-testid="general-metatags_attributes_value"
-                                               class="p-inputtext-sm"
-                                    />
-                                    <Button label="Content" disabled="" />
-                                    <InputText v-model="item.value.content"
-                                               data-testid="general-metatags_attributes_content"></InputText>
-                                    <Button icon="pi pi-trash"
-                                            data-testid="general-remove_tag"
-                                            @click="store.removeMetaTags(item)"/>
-                                </div>
-                            </div>
-                            <div class="col-12 md:col-8">
-                                <div class="p-inputgroup">
-                                    <Button icon="pi pi-plus"
-                                            data-testid="general-add_newtag"
-                                            @click="store.addMetaTags"
-                                            label="Add Meta Tag"></Button>
-                                    <Button label="Save"
-                                            @click="store.storeTags"
-                                            data-testid="general-meta_tag-save" ></Button>
-                                    <Button icon="pi pi-copy"
-                                            data-testid="general-meta_tag_copy"
-                                            @click="store.getCopy('meta_tags')"></Button>
-                                </div>
-                            </div>
-                            <div class="col-12 md:col-4">
-                                <div class="p-inputgroup">
-                                    <Dropdown v-model="store.tag_type"
-                                              :options="[
-                                                  {name:'Google Webmaster',value:'google-webmaster'},
-                                                  {name:'Open Graph (Facebook)',value:'open-graph'},
-                                              ]"
-                                              data-testid="general-gegnerate_tag"
-                                              optionLabel="name"
-                                              optionValue="value"
-                                              placeholder="Select a type"/>
-                                    <Button label="Generate" @click="store.generateTags"></Button>
-                                </div>
-                            </div>
-                        </div>
+                        <MetaTags />
                     </AccordionTab>
                 </Accordion>
             </template>
