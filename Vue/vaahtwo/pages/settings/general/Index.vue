4d74a14e59bc514ca9a7481578244bca04171b72
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2023-02-15 15:46:34 +0530

    Updated: shift date time settings on a separate component

--- Vue/vaahtwo/pages/settings/general/Index.vue
+++ Vue/vaahtwo/pages/settings/general/Index.vue
@@ -1,11 +1,12 @@
 <script setup>
-import {onMounted, reactive, ref} from "vue";
-import {useRoute} from 'vue-router';
+import { onMounted } from "vue";
+import { useRoute } from 'vue-router';
 import draggable from 'vuedraggable';
 import { vaah } from '../../../vaahvue/pinia/vaah';
 import SiteSettings from './components/SiteSettings.vue';
+import DateTime from './components/DateTime.vue';
 
-import {useGeneralStore} from "../../../stores/settings/store-general_setting";
+import { useGeneralStore } from "../../../stores/settings/store-general_setting";
 
 const store = useGeneralStore();
 const route = useRoute();
@@ -64,82 +65,7 @@ onMounted(async () => {
                             </div>
                         </template>
 
-                        <div v-if="store.list" class="grid">
-                            <div class="col-4">
-                                <h5 class="p-1 text-xs mb-1">Date Format</h5>
-                                <div class="p-inputgroup">
-                                    <Dropdown v-model="store.list.date_format"
-                                              data-testid="general-date_format"
-                                              :options="store.date_format_options"
-                                    />
-
-                                    <InputText placeholder="Enter Custom date format"
-                                               v-model="store.list.date_format_custom"
-                                               data-testid="general-date_format_custom"
-                                               v-if="store.list.date_format === 'custom'"
-                                               class="p-inputtext-sm"
-                                    />
-
-                                    <Button icon="pi pi-copy"
-                                            data-testid="general-date_format_copy"
-                                            @click="store.getCopy('date_format')"
-                                            class="p-button-sm"
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="col-4">
-                                <h5 class="p-1 text-xs mb-1">Time Format</h5>
-                                <div class="p-inputgroup">
-                                    <Dropdown v-model="store.list.time_format"
-                                              data-testid="general-time_format"
-                                              :options="store.time_format_options"
-                                    />
-
-                                    <InputText placeholder="Enter Custom time format"
-                                               v-model="store.list.time_format_custom"
-                                               data-testid="general-time_format_custom"
-                                               v-if="store.list.time_format === 'custom'"
-                                               class="p-inputtext-sm"
-                                    />
-                                    <Button icon="pi pi-copy"
-                                            data-testid="general-time_format_copy"
-                                            @click="store.getCopy('time_format')"
-                                            class="p-button-sm"
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="col-4">
-                                <h5 class="p-1 text-xs mb-1">Date Time Format</h5>
-                                <div class="p-inputgroup">
-                                    <Dropdown v-model="store.list.datetime_format"
-                                              data-testid="general-datetime_format"
-                                              :options="store.date_time_format_options"
-                                    />
-
-                                    <InputText placeholder="Enter Custom date-time format"
-                                               v-model="store.list.datetime_format_custom"
-                                               data-testid="general-datetime_format_custom"
-                                               v-if="store.list.datetime_format === 'custom'"
-                                    />
-
-                                    <Button icon="pi pi-copy"
-                                            data-testid="general-datetime_format_copy"
-                                            @click="store.getCopy('datetime_format')"
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="col-12 mt-5">
-                                <Button label="Save Settings"
-                                        @click="store.storeSiteSettings()"
-                                        data-testid="general-date_format_save"
-                                        icon="pi pi-save"
-                                        class="mr-2 p-button-sm"
-                                />
-                            </div>
-                        </div>
+                        <DateTime />
                     </AccordionTab>
 
                     <AccordionTab>
