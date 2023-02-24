50e10815bf5741ad3805d1b1231cbd0947c16f64
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2023-02-24 19:01:43 +0530

    Updated: try catch on RegistrationsController

--- Http/Controllers/Backend/RegistrationsController.php
+++ Http/Controllers/Backend/RegistrationsController.php
@@ -1,142 +1,303 @@
-<?php namespace WebReinvent\VaahCms\Http\Controllers\Backend;
+<?php
 
+namespace WebReinvent\VaahCms\Http\Controllers\Backend;
+
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+
 use WebReinvent\VaahCms\Entities\Setting;
 use WebReinvent\VaahCms\Models\TaxonomyBase;
 use WebReinvent\VaahCms\Models\User;
 use WebReinvent\VaahCms\Models\Registration;
 
-
 class RegistrationsController extends Controller
 {
-
-
     //----------------------------------------------------------
     public function __construct()
     {
-
     }
-
     //----------------------------------------------------------
-
-    public function getAssets(Request $request)
+    public function getAssets(Request $request): JsonResponse
     {
+        if (!Auth::user()->hasPermission('has-access-of-registrations-section')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
-        $data = [];
-
-        $data['permission'] = [];
-        $data['rows'] = config('vaahcms.per_page');
-
-        $data['fillable']['except'] = [
-            'uuid',
-            'created_by',
-            'updated_by',
-            'deleted_by',
-        ];
-
-
-        $model = new Registration();
-        $fillable = $model->getFillable();
-        $data['fillable']['columns'] = array_diff(
-            $fillable, $data['fillable']['except']
-        );
-
-        foreach ($fillable as $column) {
-            $data['empty_item'][$column] = null;
+            return response()->json($response);
         }
 
-        $custom_fields = Setting::query()->where('category','user_setting')
-            ->where('label','custom_fields')->first();
+        try {
+            $data = [];
 
-        if (isset($custom_fields)) {
-            foreach ($custom_fields['value'] as $custom_field) {
-                $data['empty_item']['meta'][$custom_field->slug] = null;
+            $data['permission'] = [];
+            $data['rows'] = config('vaahcms.per_page');
+
+            $data['fillable']['except'] = [
+                'uuid',
+                'created_by',
+                'updated_by',
+                'deleted_by',
+            ];
+
+
+            $model = new Registration();
+            $fillable = $model->getFillable();
+            $data['fillable']['columns'] = array_diff(
+                $fillable, $data['fillable']['except']
+            );
+
+            foreach ($fillable as $column) {
+                $data['empty_item'][$column] = null;
+            }
+
+            $custom_fields = Setting::query()->where('category','user_setting')
+                ->where('label','custom_fields')->first();
+
+            if (isset($custom_fields)) {
+                foreach ($custom_fields['value'] as $custom_field) {
+                    $data['empty_item']['meta'][$custom_field->slug] = null;
+                }
+            }
+
+            $data['actions'] = [];
+
+            $data['country_calling_code'] = vh_get_country_list();
+            $data['countries'] = vh_get_country_list();
+            $data['timezones'] = vh_get_timezones();
+            $data['country_code'] = vh_get_country_list();
+            $data['registration_statuses'] = TaxonomyBase::getTaxonomyByType('registrations');
+            $data['bulk_actions'] = vh_general_bulk_actions();
+            $data['name_titles'] = vh_name_titles();
+            $data['fields'] = User::getUserSettings();
+            $data['custom_fields'] = $custom_fields;
+
+            $response['success'] = true;
+            $response['data'] = $data;
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
             }
         }
 
-        $data['actions'] = [];
+        return response()->json($response);
+    }
+    //----------------------------------------------------------
+    public function getList(Request $request): JsonResponse
+    {
+        if (!Auth::user()->hasPermission('has-access-of-registrations-section')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
-//      field copied from Http/Registration controller 's assets methods
-        $data['country_calling_code'] = vh_get_country_list();
-        $data['countries'] = vh_get_country_list();
-        $data['timezones'] = vh_get_timezones();
-        $data['country_code'] = vh_get_country_list();
-        $data['registration_statuses'] = TaxonomyBase::getTaxonomyByType('registrations');
-        $data['bulk_actions'] = vh_general_bulk_actions();
-        $data['name_titles'] = vh_name_titles();
-        $data['fields'] = User::getUserSettings();
-        $data['custom_fields'] = $custom_fields;
-        //      field copied from Http/Registration controller 's assets methods
+            return response()->json($response);
+        }
 
+        try {
+            $response= Registration::getList($request);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
 
-        $response['success'] = true;
-        $response['data'] = $data;
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
 
-        return $response;
+        return response()->json($response);
     }
+    //----------------------------------------------------------
+    public function updateList(Request $request): JsonResponse
+    {
+        try {
+            $response = Registration::updateList($request);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
 
-    //----------------------------------------------------------
-    public function getList(Request $request)
-    {
-        return Registration::getList($request);
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function updateList(Request $request)
+    public function listAction(Request $request, $type): JsonResponse
     {
-        return Registration::updateList($request);
+        try {
+            $response= Registration::listAction($request, $type);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function listAction(Request $request, $type)
+    public function deleteList(Request $request): JsonResponse
     {
-        return Registration::listAction($request, $type);
+        try {
+            $response = Registration::deleteList($request);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function deleteList(Request $request)
+    public function createItem(Request $request): JsonResponse
     {
-        return Registration::deleteList($request);
+        try {
+            $response = Registration::createItem($request);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function createItem(Request $request)
+    public function getItem(Request $request, $id): JsonResponse
     {
-        return Registration::createItem($request);
+        try {
+            $response =  Registration::getItem($id);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function getItem(Request $request, $id)
+    public function updateItem(Request $request ,$id): JsonResponse
     {
-        return Registration::getItem($id);
+        try {
+            $response = Registration::updateItem($request, $id);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function updateItem(Request $request,$id)
+    public function deleteItem(Request $request,$id): JsonResponse
     {
-        return Registration::updateItem($request,$id);
+        try {
+            $response = Registration::deleteItem($request, $id);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function deleteItem(Request $request,$id)
+    public function itemAction(Request $request, $id, $action): JsonResponse
     {
-        return Registration::deleteItem($request,$id);
+        try {
+            $response = Registration::itemAction($request, $id, $action);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function itemAction(Request $request,$id,$action)
+    public function sendVerificationEmail(Request $request,$id): JsonResponse
     {
-        return Registration::itemAction($request,$id,$action);
+        try {
+            $response = Registration::sendVerificationEmail($request, $id);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function sendVerificationEmail(Request $request,$id)
+    public function createUser(Request $request,$id): JsonResponse
     {
-        return Registration::sendVerificationEmail($request, $id);
-    }
-    public function createUser(Request $request,$id)
-    {
-        if(!\Auth::user()->hasPermission('can-create-users-from-registrations'))
-        {
+        if (!Auth::user()->hasPermission('can-create-users-from-registrations')) {
             $response['status'] = 'failed';
             $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
-            return $response;
+            return response()->json($response);
         }
 
         $response = Registration::createUser($id);
+
         return response()->json($response);
     }
     //----------------------------------------------------------
