501b6ccc0a482ce2bdbcbfa109752c14eade5663
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2023-02-25 13:01:13 +0530

    Updated: PermissionsController's method with try catch

--- Http/Controllers/Backend/PermissionsController.php
+++ Http/Controllers/Backend/PermissionsController.php
@@ -1,105 +1,330 @@
-<?php namespace WebReinvent\VaahCms\Http\Controllers\Backend;
+<?php
 
+namespace WebReinvent\VaahCms\Http\Controllers\Backend;
+
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use WebReinvent\VaahCms\Models\Permission;
+use Illuminate\Support\Facades\Auth;
 
+use WebReinvent\VaahCms\Models\Permission;
 
 class PermissionsController extends Controller
 {
-
-
     //----------------------------------------------------------
     public function __construct()
     {
-
     }
-
     //----------------------------------------------------------
-
-    public function getAssets(Request $request)
+    public function getAssets(Request $request): JsonResponse
     {
+        if (!Auth::user()->hasPermission('has-access-of-permissions-section')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
-        $data = [];
+            return response()->json($response);
+        }
+        try {
+            $data = [];
 
-        $data['permission'] = [];
-        $data['rows'] = config('vaahcms.per_page');
+            $data['permission'] = [];
+            $data['rows'] = config('vaahcms.per_page');
 
-        $data['fillable']['except'] = [
-            'uuid',
-            'created_by',
-            'updated_by',
-            'deleted_by',
-        ];
+            $data['fillable']['except'] = [
+                'uuid',
+                'created_by',
+                'updated_by',
+                'deleted_by',
+            ];
 
-        $model = new Permission();
-        $fillable = $model->getFillable();
-        $data['fillable']['columns'] = array_diff(
-            $fillable, $data['fillable']['except']
-        );
+            $model = new Permission();
+            $fillable = $model->getFillable();
+            $data['fillable']['columns'] = array_diff(
+                $fillable, $data['fillable']['except']
+            );
 
-        foreach ($fillable as $column)
-        {
-            $data['empty_item'][$column] = null;
+            foreach ($fillable as $column)
+            {
+                $data['empty_item'][$column] = null;
+            }
+
+            $data['actions'] = [];
+
+            $response['success'] = true;
+            $response['data'] = $data;
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
         }
 
-        $data['actions'] = [];
+        return response()->json($response);
+    }
+    //----------------------------------------------------------
+    public function getList(Request $request): JsonResponse
+    {
+        if (!Auth::user()->hasPermission('can-read-permissions')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
-        $response['success'] = true;
-        $response['data'] = $data;
+            return response()->json($response);
+        }
 
-        return $response;
-    }
+        try {
+            $response = Permission::getList($request);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
 
-    //----------------------------------------------------------
-    public function getList(Request $request)
-    {
-        return Permission::getList($request);
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function updateList(Request $request)
+    public function updateList(Request $request): JsonResponse
     {
-        return Permission::updateList($request);
+        if (!Auth::user()->hasPermission('can-update-permissions')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
+
+            return response()->json($response);
+        }
+
+        try {
+            $response = Permission::updateList($request);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function listAction(Request $request, $type)
+    public function listAction(Request $request, $type): JsonResponse
     {
-        return Permission::listAction($request, $type);
+        if (!Auth::user()->hasPermission('can-update-permissions')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
+
+            return response()->json($response);
+        }
+
+        try {
+            $response = Permission::listAction($request, $type);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function deleteList(Request $request)
+    public function deleteList(Request $request): JsonResponse
     {
-        return Permission::deleteList($request);
+        if (!Auth::user()->hasPermission('can-update-permissions')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
+
+            return response()->json($response);
+        }
+
+        try {
+            $response = Permission::deleteList($request);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function createItem(Request $request)
+    public function createItem(Request $request): JsonResponse
     {
-        return Permission::createItem($request);
+        if (!Auth::user()->hasPermission('can-update-permissions')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
+
+            return response()->json($response);
+        }
+
+        try {
+            $response = Permission::createItem($request);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function getItem(Request $request, $id)
+    public function getItem(Request $request, $id): JsonResponse
     {
-        return Permission::getItem($id);
+        if (!Auth::user()->hasPermission('can-update-permissions')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
+
+            return response()->json($response);
+        }
+
+        try {
+            $response = Permission::getItem($id);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function updateItem(Request $request,$id)
+    public function updateItem(Request $request, $id): JsonResponse
     {
-        return Permission::updateItem($request,$id);
+        if (!Auth::user()->hasPermission('can-update-permissions')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
+
+            return response()->json($response);
+        }
+
+        try {
+            $response = Permission::updateItem($request, $id);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function deleteItem(Request $request,$id)
+    public function deleteItem(Request $request, $id): JsonResponse
     {
-        return Permission::deleteItem($request,$id);
+        if (!Auth::user()->hasPermission('can-update-permissions')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
+
+            return response()->json($response);
+        }
+
+        try {
+            $response = Permission::deleteItem($request, $id);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function itemAction(Request $request,$id,$action)
+    public function itemAction(Request $request, $id, $action): JsonResponse
     {
-        return Permission::itemAction($request,$id,$action);
+        if (!Auth::user()->hasPermission('can-update-permissions')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
+
+            return response()->json($response);
+        }
+
+        try {
+            $response = Permission::itemAction($request, $id, $action);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function getItemRoles(Request $request, $id)
+    public function getItemRoles(Request $request, $id): JsonResponse
     {
-        return Permission::getItemRoles($request, $id);
+        if (!Auth::user()->hasPermission('can-update-permissions')) {
+            $response['success'] = false;
+            $response['errors'][] = trans("vaahcms::messages.permission_denied");
+
+            return response()->json($response);
+        }
+
+        try {
+            $response = Permission::getItemRoles($request, $id);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
 }
