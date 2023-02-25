3e3aa8ad789fd74fa546de6c07f34d0dc3e079bd
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2023-02-25 17:27:18 +0530

    Updated: UpdateController's method's using try catch

--- Http/Controllers/Backend/Settings/UpdateController.php
+++ Http/Controllers/Backend/Settings/UpdateController.php
@@ -2,7 +2,7 @@
 
 namespace WebReinvent\VaahCms\Http\Controllers\Backend\Settings;
 
-
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Process\Process;
+
 use WebReinvent\VaahCms\Entities\Notification;
 use WebReinvent\VaahCms\Entities\NotificationContent;
 use WebReinvent\VaahCms\Entities\Notified;
@@ -20,79 +21,82 @@ use WebReinvent\VaahCms\Libraries\VaahSetup;
 use WebReinvent\VaahExtend\Libraries\VaahArtisan;
 use WebReinvent\VaahCms\Models\Role;
 
-
 class UpdateController extends Controller
 {
-
     public $process;
 
     //----------------------------------------------------------
     public function __construct()
     {
-
     }
-
     //----------------------------------------------------------
-    public function storeUpdate(Request $request)
+    public function storeUpdate(Request $request): JsonResponse
     {
-
-        if(!\Auth::user()->hasPermission('has-access-of-setting-section'))
-        {
+        if (!Auth::user()->hasPermission('has-access-of-setting-section')) {
             $response['success'] = false;
             $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
             return response()->json($response);
         }
 
-        $settings = [
-            [
-                "category"=>'global',
-                "key"=> 'update_checked_at',
-                "value"=> \Carbon::now()->format('Y-m-d H:i:s'),
-            ],
-            [
-                "category"=>'global',
-                "key"=> 'is_update_available',
-                "value"=> $request->update_available,
-            ],
-            [
-                "category"=>'global',
-                "key"=> 'latest_remote_version',
-                "value"=> $request->remote_version,
-            ],
-        ];
+        try {
+            $settings = [
+                [
+                    "category"=>'global',
+                    "key"=> 'update_checked_at',
+                    "value"=> \Carbon::now()->format('Y-m-d H:i:s'),
+                ],
+                [
+                    "category"=>'global',
+                    "key"=> 'is_update_available',
+                    "value"=> $request->update_available,
+                ],
+                [
+                    "category"=>'global',
+                    "key"=> 'latest_remote_version',
+                    "value"=> $request->remote_version,
+                ],
+            ];
 
-        foreach ($settings as $setting)
-        {
-            $stored_settings = Setting::where('category', $setting['category'])
-            ->where('key', $setting['key'])
-            ->first();
-
-            if(!$stored_settings)
+            foreach ($settings as $setting)
             {
-                $stored_settings = new Setting();
+                $stored_settings = Setting::where('category', $setting['category'])
+                    ->where('key', $setting['key'])
+                    ->first();
+
+                if(!$stored_settings)
+                {
+                    $stored_settings = new Setting();
+                }
+
+                $stored_settings->fill($setting);
+                $stored_settings->save();
             }
 
-            $stored_settings->fill($setting);
-            $stored_settings->save();
+            if($request->has('update_available') && $request->update_available){
+                self::createBackendNotificationForUpdate($request);
+            }
+
+            $response['success'] = true;
+            $response['data'][] = '';
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
         }
 
-        if($request->has('update_available') && $request->update_available){
-            self::createBackendNotificationForUpdate($request);
-        }
-
-        $response['success'] = true;
-        $response['data'][] = '';
-        return $response;
-
-
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function upgrade()
+    public function upgrade(): JsonResponse
     {
-
-        if(!\Auth::user()->hasPermission('has-access-of-setting-section'))
-        {
+        if(!Auth::user()->hasPermission('has-access-of-setting-section')) {
             $response['success'] = false;
             $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
@@ -100,20 +104,19 @@ class UpdateController extends Controller
         }
 
         try{
-            return $this->runCommand("composer", "update");
-        }catch(\Exception $e)
-        {
+            $response = $this->runCommand("composer", "update");
+        } catch(\Exception $e) {
             $response['success'] = false;
             $response['errors'][] = $e->getMessage();
-            return $response;
         }
+
+        return response()->json($response);
     }
     //----------------------------------------------------------
     public function publish()
     {
 
-        if(!\Auth::user()->hasPermission('has-access-of-setting-section'))
-        {
+        if(!Auth::user()->hasPermission('has-access-of-setting-section')) {
             $response['success'] = false;
             $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
@@ -121,7 +124,6 @@ class UpdateController extends Controller
         }
 
         try{
-
             //publish assets
             VaahSetup::publishAssets();
 
@@ -132,34 +134,29 @@ class UpdateController extends Controller
             $provider = "WebReinvent\VaahCms\VaahCmsServiceProvider";
             $response = VaahArtisan::publishMigrations($provider);
 
-            if(isset($response['success']) && !$response['success'])
-            {
+            if (isset($response['success']) && !$response['success']) {
                 return $response;
             }
 
             //publish vaahcms seeds
             $response = VaahArtisan::publishSeeds($provider);
-            if(isset($response['success']) && !$response['success'])
-            {
+            if (isset($response['success']) && !$response['success']) {
                 return $response;
             }
 
-            $res['success'] = true;
-            return $res;
-        }catch(\Exception $e)
-        {
+            $response['success'] = true;
+        } catch(\Exception $e) {
             $response['success'] = false;
             $response['errors'][] = $e->getMessage();
-            return $response;
         }
 
+        return response()->json($response);
     }
     //----------------------------------------------------------
     public function runMigrations()
     {
 
-        if(!\Auth::user()->hasPermission('has-access-of-setting-section'))
-        {
+        if (!Auth::user()->hasPermission('has-access-of-setting-section')) {
             $response['success'] = false;
             $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
@@ -167,182 +164,210 @@ class UpdateController extends Controller
         }
 
         try{
-
             self::setVaahCmsVersionInEnv();
 
             //run migration
             $response = VaahArtisan::migrate();
-            if(isset($response['success']) && !$response['success'])
-            {
+            if (isset($response['success']) && !$response['success']) {
                 return $response;
             }
 
             //run vaahcms seeds
             $seed_class = "WebReinvent\VaahCms\Database\Seeders\VaahCmsTableSeeder";
             $response = VaahArtisan::seed('db:seed', $seed_class);
-            if(isset($response['success']) && !$response['success'])
-            {
+            if (isset($response['success']) && !$response['success']) {
                 return $response;
             }
 
-            $res['success'] = true;
-            return $res;
-        }catch(\Exception $e)
-        {
+            $response['success'] = true;
+        } catch (\Exception $e) {
             $response['success'] = false;
             $response['errors'][] = $e->getMessage();
-            return $response;
         }
 
+        return response()->json($response);
     }
     //----------------------------------------------------------
-    public function clearCache()
+    public function clearCache(): JsonResponse
     {
 
-        if(!\Auth::user()->hasPermission('has-access-of-setting-section'))
-        {
+        if (!Auth::user()->hasPermission('has-access-of-setting-section')) {
             $response['success'] = false;
             $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
             return response()->json($response);
         }
 
-        try{
+        try {
             VaahArtisan::clearCache();
 
             $notification = Notification::where('slug','send-update-message')->first();
 
-            Notified::where('vh_notification_id',$notification->id)->forceDelete();
+            Notified::query()->where('vh_notification_id',$notification->id)->forceDelete();
 
-            $response['status'] = "success";
-            return $response;
-        }catch(\Exception $e)
+            $response['success'] = true;
+        } catch (\Exception $e)
         {
             $response['success'] = false;
             $response['errors'][] = $e->getMessage();
-            return $response;
         }
 
+        return response()->json($response);
     }
     //----------------------------------------------------------
     public function createBackendNotificationForUpdate($request)
     {
+        try {
+            $notification = Notification::where('slug','send-update-message')->first();
 
-        $notification = Notification::where('slug','send-update-message')->first();
+            if(!$notification){
+                $notification = new Notification();
+                $notification->slug = 'send-update-message';
+                $notification->name = 'Send Update Message';
+                $notification->via_backend = '1';
+                $notification->save();
+            }
 
-        if(!$notification){
-            $notification = new Notification();
-            $notification->slug = 'send-update-message';
-            $notification->name = 'Send Update Message';
-            $notification->via_backend = '1';
-            $notification->save();
+            $message = 'A newer version '.$request->remote_version.' of VaahCMS is available.';
+            $label = 'Go to Update';
+            $link = route('vh.backend').'#/vaah/settings/update';
+
+            if($request->has('manual_update') && $request->manual_update){
+                $message = $message . ' This is a major release. You have to do manual upgrade to update VaahCms.';
+            }
+
+            $translated = [
+                "message" => $message,
+                "action" => [
+                    "label" => $label,
+                    "link" => $link
+                ],
+            ];
+
+            $notified = Notified::where('vh_notification_id',$notification->id)
+                ->where('via','backend')
+                ->where('vh_user_id',Auth::user()->id)->first();
+
+            if(!$notified){
+                $notified = new Notified();
+                $notified->vh_notification_id = $notification->id;
+                $notified->vh_user_id = Auth::user()->id;
+                $notified->via = 'backend';
+            }
+
+            $notified->meta = $translated;
+            $notified->last_attempt_at = null;
+            $notified->sent_at = null;
+            $notified->read_at = null;
+            $notified->marked_delivered = null;
+
+            $notified->save();
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
         }
-
-        $message = 'A newer version '.$request->remote_version.' of VaahCMS is available.';
-        $label = 'Go to Update';
-        $link = route('vh.backend').'#/vaah/settings/update';
-
-        if($request->has('manual_update') && $request->manual_update){
-            $message = $message . ' This is a major release. You have to do manual upgrade to update VaahCms.';
-        }
-
-
-        $translated = [
-            "message" => $message,
-            "action" => [
-                "label" => $label,
-                "link" => $link
-            ],
-        ];
-
-        $notified = Notified::where('vh_notification_id',$notification->id)
-            ->where('via','backend')
-            ->where('vh_user_id',Auth::user()->id)->first();
-
-        if(!$notified){
-            $notified = new Notified();
-            $notified->vh_notification_id = $notification->id;
-            $notified->vh_user_id = Auth::user()->id;
-            $notified->via = 'backend';
-        }
-
-        $notified->meta = $translated;
-        $notified->last_attempt_at = null;
-        $notified->sent_at = null;
-        $notified->read_at = null;
-        $notified->marked_delivered = null;
-
-        $notified->save();
-
     }
     //----------------------------------------------------------
     public static function setVaahCmsVersionInEnv()
     {
 
-        $reflector = new \ReflectionClass(\WebReinvent\VaahCms\VaahCmsServiceProvider::class);
+        try {
+            $reflector = new \ReflectionClass(\WebReinvent\VaahCms\VaahCmsServiceProvider::class);
 
-        $ref_path = str_replace("VaahCmsServiceProvider.php","",$reflector->getFileName());
+            $ref_path = str_replace("VaahCmsServiceProvider.php","",$reflector->getFileName());
 
-        $path =$ref_path .'composer.json';
+            $path =$ref_path .'composer.json';
 
-        $config_data = json_decode(file_get_contents($path), true);
+            $config_data = json_decode(file_get_contents($path), true);
 
-        $env_list = VaahSetup::getEnvFileVariables(null, 'list');
+            $env_list = VaahSetup::getEnvFileVariables(null, 'list');
 
-        $env_config_exist = false;
+            $env_config_exist = false;
 
-        foreach ($env_list as $key => $item){
-            if($item['key'] === 'VAAHCMS_VERSION'){
-                $env_config_exist = true;
-                $env_list[$key]['value'] = $config_data['version'];
+            foreach ($env_list as $key => $item){
+                if($item['key'] === 'VAAHCMS_VERSION'){
+                    $env_config_exist = true;
+                    $env_list[$key]['value'] = $config_data['version'];
+                }
             }
+
+            if(!$env_config_exist){
+                $env_list[] = [
+                    'key' => 'VAAHCMS_VERSION',
+                    'value' => $config_data['version']
+                ];
+            }
+
+            $request = new Request($env_list);
+
+            VaahSetup::generateEnvFile($request, 'list');
+
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+
+            return response()->json($response);
         }
-
-        if(!$env_config_exist){
-            $env_list[] = [
-                'key' => 'VAAHCMS_VERSION',
-                'value' => $config_data['version']
-            ];
-        }
-
-        $request = new Request($env_list);
-
-        VaahSetup::generateEnvFile($request, 'list');
-
     }
     //----------------------------------------------------------
     /*
      * $executor like "composer", "php", "npm" etc
      * $command lik "install", "upgrade", "update"
      */
-    public function runCommand($executor, $command)
+    public function runCommand($executor, $command): JsonResponse
     {
 
-        $buffer = null;
-        $output = null;
+        try {
+            $buffer = null;
+            $output = null;
 
-        $this->process = new Process([$executor, $command]);
+            $this->process = new Process([$executor, $command]);
 
-        if($executor == 'composer')
-        {
-            $this->process->setEnv(['COMPOSER_HOME' => base_path('vendor/bin/composer')]);
-            $this->process->setWorkingDirectory(base_path('/'));
+            if($executor == 'composer')
+            {
+                $this->process->setEnv(['COMPOSER_HOME' => base_path('vendor/bin/composer')]);
+                $this->process->setWorkingDirectory(base_path('/'));
+            }
+            $this->process->run();
+
+            if (!$this->process->isSuccessful()) {
+                $response['status'] = "failed";
+                $output .= $this->process->getErrorOutput();
+            } else{
+                $response['status'] = "success";
+                $output .= $this->process->getOutput();
+            }
+
+
+            $response['data']['buffer'] = $buffer;
+            $response['data']['output'] = $output;
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
         }
-        $this->process->run();
 
-        if (!$this->process->isSuccessful()) {
-            $response['status'] = "failed";
-            $output .= $this->process->getErrorOutput();
-        } else{
-            $response['status'] = "success";
-            $output .= $this->process->getOutput();
-        }
-
-
-        $response['data']['buffer'] = $buffer;
-        $response['data']['output'] = $output;
-
-        return $response;
+        return response()->json($response);
     }
     //----------------------------------------------------------
 
