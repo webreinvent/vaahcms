6e445e0a77084978d28460787d063d61cd31460f
Tanmoy Dey <tanmoy.d001@webreinvent.com>
2023-02-25 16:03:12 +0530

    Updated: EveController's methods using try catch

--- Http/Controllers/Backend/Settings/EnvController.php
+++ Http/Controllers/Backend/Settings/EnvController.php
@@ -3,31 +3,24 @@
 namespace WebReinvent\VaahCms\Http\Controllers\Backend\Settings;
 
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
-use WebReinvent\VaahCms\Entities\Language;
-use WebReinvent\VaahCms\Entities\Setting;
-use WebReinvent\VaahCms\Libraries\VaahBackup;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
 use WebReinvent\VaahCms\Libraries\VaahHelper;
 use WebReinvent\VaahCms\Libraries\VaahSetup;
-use WebReinvent\VaahCms\Models\Role;
-
 
 class EnvController extends Controller
 {
-
     //----------------------------------------------------------
     public function __construct()
     {
-
     }
-
     //----------------------------------------------------------
-    public function getAssets(Request $request)
+    public function getAssets(Request $request): JsonResponse
     {
 
         if(!\Auth::user()->hasPermission('has-access-of-setting-section'))
@@ -38,91 +31,126 @@ class EnvController extends Controller
             return response()->json($response);
         }
 
+        try {
+            $data['is_installed'] = VaahSetup::isInstalled();
+            $data['environments'] = vh_environments();
+            $data['timezones'] = vh_get_timezones();
+            $data['database_types'] = vh_database_types();
+            $data['mail_encryption_types'] = vh_mail_encryption_types();
+            $data['mail_sample_settings'] = vh_mail_sample_settings();
+            $data['country_calling_codes'] = vh_get_countries_calling_codes();
+            $data['app_url'] = url("/");
 
-        $data['is_installed'] = VaahSetup::isInstalled();
-        $data['environments'] = vh_environments();
-        $data['timezones'] = vh_get_timezones();
-        $data['database_types'] = vh_database_types();
-        $data['mail_encryption_types'] = vh_mail_encryption_types();
-        $data['mail_sample_settings'] = vh_mail_sample_settings();
-        $data['country_calling_codes'] = vh_get_countries_calling_codes();
-        $data['app_url'] = url("/");
+            $response['success'] = true;
+            $response['data'] = $data;
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
 
-        $response['success'] = true;
-        $response['data'] = $data;
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
 
         return response()->json($response);
     }
     //----------------------------------------------------------
-    public function getList(Request $request)
+    public function getList(Request $request): JsonResponse
     {
 
-        if(!\Auth::user()->hasPermission('has-access-of-setting-section'))
-        {
+        if (!Auth::user()->hasPermission('has-access-of-setting-section')) {
             $response['success'] = false;
             $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
             return response()->json($response);
         }
 
-        $data = [];
+        try {
+            $data = [];
 
-        $data['env_file'] = VaahSetup::getActiveEnvFileName();
-        $data['list'] = VaahSetup::getEnvFileVariables(null, 'list');
+            $data['env_file'] = VaahSetup::getActiveEnvFileName();
+            $data['list'] = VaahSetup::getEnvFileVariables(null, 'list');
 
-        $response['success'] = true;
-        $response['data'] = $data;
+            $response['success'] = true;
+            $response['data'] = $data;
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
 
         return response()->json($response);
-
     }
     //----------------------------------------------------------
-    public function downloadFile(Request $request,$file_name)
+    public function downloadFile(Request $request, $file_name): BinaryFileResponse | string
     {
 
-        if(!\Auth::user()->hasPermission('has-access-of-setting-section'))
-        {
+        if (!Auth::user()->hasPermission('has-access-of-setting-section')) {
             $response['success'] = false;
             $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
             return response()->json($response);
         }
 
-        if(!$file_name || !File::exists(base_path('/'.$file_name))){
-            return 'No File Found.';
-        }
+        try {
+            if(!$file_name || !File::exists(base_path('/'.$file_name))){
+                return 'No File Found.';
+            }
 
-        $file_path =  base_path('/'.$file_name);
+            $file_path =  base_path('/'.$file_name);
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
+        }
 
         return response()->download($file_path);
-
     }
-
     //----------------------------------------------------------
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-
-        if(!\Auth::user()->hasPermission('has-access-of-setting-section'))
-        {
+        if (!Auth::user()->hasPermission('has-access-of-setting-section')) {
             $response['success'] = false;
             $response['errors'][] = trans("vaahcms::messages.permission_denied");
 
             return response()->json($response);
         }
 
-        $response = VaahSetup::generateEnvFile($request, 'list');
+        try {
+            $response = VaahSetup::generateEnvFile($request, 'list');
 
-        if(isset($response['success']) && $response['success'])
-        {
-            VaahHelper::clearCache();
-            $response['data']['redirect_url'] = route('vh.backend');
+            if (isset($response['success']) && $response['success']) {
+                VaahHelper::clearCache();
+                $response['data']['redirect_url'] = route('vh.backend');
+            }
+        } catch (\Exception $e) {
+            $response = [];
+            $response['success'] = false;
+
+            if (env('APP_DEBUG')) {
+                $response['errors'][] = $e->getMessage();
+                $response['hint'][] = $e->getTrace();
+            } else {
+                $response['messages'][] = 'Something went wrong.';
+            }
         }
 
         return response()->json($response);
-
     }
     //----------------------------------------------------------
-    //----------------------------------------------------------
-
-
 }
