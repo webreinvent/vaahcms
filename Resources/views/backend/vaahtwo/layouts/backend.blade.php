43a2dd95c6ce1558f7982ff6055ff2741876e6c9
Vikram <vikram.wri231@webreinvent.com>
2023-03-16 19:05:24 +0530

    Added: backend blade file

---
+++ Resources/views/backend/vaahtwo/layouts/backend.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>
+        <?php
+        if(isset($data->title)) { echo $data->title; }
+        elseif(env('VAAHCMS_VERSION')) {
+            echo config('vaahcms.app_name')." v".env('VAAHCMS_VERSION');
+        }else{
+            echo config('vaahcms.app_name')." v".config('vaahcms.version');
+        }
+        ?>
+    </title>
+
+    <meta charset="UTF-8">
+
+    <meta name=description content="">
+
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" id="_token" content="{{ csrf_token() }}">
+
+    <meta name="current-url" id="current_url" content="{{ url()->current() }}">
+    <meta name="debug" id="debug" content="{{config('vaahcms.debug')}}">
+    <meta name="timezone" id="app_timezone" content="{{env('APP_TIMEZONE')??'UTC'}}">
+
+
+    @if(env('SENTRY_DSN'))
+    <meta name="sentry-dns" id="sentry_dns" content="{{env('SENTRY_DSN')}}">
+    @endif
+
+    <base href="{{\URL::to('/')}}">
+
+    <link href="https:/./fonts.googleapis.com/css?family=IBM+Plex+Sans:300,400,500,600,700&display=swap" rel="stylesheet">
+
+    {!! vh_config_css() !!}
+
+    @yield('vaahcms_extend_backend_css')
+
+
+</head>
+<body class="@if(isset($data->body_class)){{$data->body_class}}@endif bulma has-background-white-bis">
+
+<div class="">
+    @include("vaahcms::backend.vaahone.components.errors")
+    @include("vaahcms::backend.vaahone.components.flash")
+
+    <div id="buefy-snackbar">
+
+    </div>
+
+</div>
+
+<div class="container-backend ">
+
+    <div id="appExtended" class="bulma" >
+
+        <sidebar :root="root"></sidebar>
+        <div v-bind:style="{ paddingLeft: root.has_padding_left }">
+            <top-menu :root="root" @sidebar-action="sidebarAction"></top-menu>
+        </div>
+
+    </div>
+
+    <!--sections-->
+    <div id="container-main" style="padding-left: 55px;">
+
+        <div id="container-apps">
+            @yield('content')
+        </div>
+
+    </div>
+    <!--sections-->
+
+</div>
+
+{!! vh_config_js() !!}
+
+@yield('vaahcms_extend_backend_js')
+
+@if(env('APP_VAAHCMS_ENV') == 'develop')
+    <script src="http://localhost:8080/vaahone/builds/app-extended.js" defer></script>
+@else
+    <script src="{{vh_get_backend_assets("builds/app-extended.js")}}" defer></script>
+@endif
+
+
+
+</body>
+</html>
