50e7cf312791095f95ecc62ddca0a4b97272daa9
we-abinash-s001 <abinash.s001@webreinvent.com>
2023-03-31 13:55:20 +0530

    Updated: module and theme migration button completed

--- Models/ModuleBase.php
+++ Models/ModuleBase.php
@@ -548,6 +548,48 @@ class ModuleBase extends Model
 
     }
     //-------------------------------------------------
+    public static function runModuleMigrations($slug)
+    {
+
+        $module = self::slug($slug)->first();
+
+        if(!isset($module->is_migratable) || (isset($module->is_migratable) && $module->is_migratable == true))
+        {
+            $module_path = config('vaahcms.modules_path').$module->name;
+            $path = vh_module_migrations_path($module->name);
+
+            $max_batch = \DB::table('migrations')
+                ->max('batch');
+
+            Migration::runMigrations($path);
+
+            $current_max_batch = \DB::table('migrations')
+                ->max('batch');
+
+            if($current_max_batch > $max_batch){
+                Migration::syncModuleMigrations($module->id,$current_max_batch);
+            }
+
+            $seeds_namespace = vh_module_database_seeder($module->name);
+            Migration::runSeeds($seeds_namespace);
+
+            //copy assets to public folder
+            Module::copyAssets($module);
+
+        }
+
+        $response['success'] = true;
+        $response['data'][] = '';
+        $response['messages'][] = 'Migration successful';
+
+        if(env('APP_DEBUG'))
+        {
+            $response['hint'][] = '';
+        }
+        return $response;
+
+    }
+    //-------------------------------------------------
     public static function deleteItem($slug)
     {
 
