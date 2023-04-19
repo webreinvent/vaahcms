9a8e47d4ddc6aef568a931a0482cb1b874e7a868
we-abinash-s001 <abinash.s001@webreinvent.com>
2023-04-19 19:07:17 +0530

    Updated: middle ware added

---
+++ Http/Middleware/VerifyAuth.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace WebReinvent\VaahCms\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Session;
+use Illuminate\Support\Facades\Auth;
+use WebReinvent\VaahCms\Entities\Theme;
+
+class VerifyAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        if(auth()->check()){
+
+            $user = auth()->user();
+
+
+            if($user->security_code)
+            {
+
+                if($user->security_code_expired_at->lt(now()))
+                {
+
+                    auth()->logout();
+
+                    return redirect()->route('vh.backend');
+                }
+
+                if(config('settings.global.mfa_status') !== 'disable'){
+
+                    if(config('settings.global.mfa_status') == 'all-users'){
+
+                        auth()->logout();
+
+                        return redirect()->route('vh.backend');
+                    }
+
+                    if(config('settings.global.mfa_status') == 'user-will-have-option'
+                        && is_array($user->mfa_methods) && count($user->mfa_methods) >= 0){
+
+                        auth()->logout();
+
+                        return redirect()->route('vh.backend');
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return $next($request);
+
+    }
+}
