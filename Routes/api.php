45cc883d116a060f89dd51e54d0c959013a27a17
we-abinash-s001 <abinash.s001@webreinvent.com>
2023-05-01 15:08:52 +0530

    Removed: register routes removed

--- Routes/api.php
+++ Routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Http\Request;
 */
 
 
-Route::post('login', [UserController::class, 'login']);
-Route::post('register', [UserController::class, 'register']);
-
-
 Route::group(
     [
         'prefix'     => 'api',
