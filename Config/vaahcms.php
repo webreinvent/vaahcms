17d2d3433988afb77c35141d8b4b71d2ac958e0e
Vikram <vikram.wri231@webreinvent.com>
2023-06-05 13:43:58 +0530

    Updated: version

--- Config/vaahcms.php
+++ Config/vaahcms.php
@@ -8,7 +8,7 @@
 $settings =  [
     'app_name' => 'VaahCMS',
     'app_slug' => 'vaahcms',
-    'version' => '2.0.3',
+    'version' => '2.0.4',
     'php_version_required' => '8.1',
     'get_config_version' => false,
     'website' => 'https://vaah.dev/cms',
