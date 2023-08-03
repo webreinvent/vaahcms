9e010c9bb1d1134f9aec82cffcd561fffe347a02
Vikram <vikram.wri231@webreinvent.com>
2023-08-03 16:52:23 +0530

    Updated: Top nav in extended view

--- Vue/vaahtwo/components/molecules/TopnavExtended.vue
+++ Vue/vaahtwo/components/molecules/TopnavExtended.vue
@@ -40,7 +40,17 @@ function getLink(item)
         </template>
 
         <template #end>
-            <div v-if="rootStore.assets.auth_user" class="flex align-items-center">
+            <div v-if="rootStore.assets.is_impersonating">
+                <div class="p-inputgroup flex-1">
+                    <Button size="small" label="Impersonating" outlined  />
+                    <InputText class="p-inputtext-sm" disabled :placeholder="rootStore.assets.auth_user.name"
+                               :value="rootStore.assets.auth_user.name" />
+                    <Button size="small" @click="rootStore.impersonateLogout()"
+                            severity="danger" label="Leave" />
+                </div>
+                <!--                <p class="text-red-500"><strong>Impersonating</strong> {{rootStore.assets.auth_user.name}}</p>-->
+            </div>
+            <div v-if="rootStore.assets.auth_user && !rootStore.assets.is_impersonating" class="flex align-items-center">
                 <a @click="toggleDropDownMenu"
                    data-testid="Topnav-Avatar"
                    class="cursor-pointer flex align-items-center">
@@ -50,36 +60,12 @@ function getLink(item)
                     <i class="pi pi-chevron-down text-sm mt-1 ml-1"></i>
                 </a>
             </div>
-            <template #end>
-                <div v-if="rootStore.assets.is_impersonating">
-                    <div class="p-inputgroup flex-1">
-                        <Button size="small" label="Impersonating" outlined  />
-                        <InputText class="p-inputtext-sm" disabled :placeholder="rootStore.assets.auth_user.name"
-                                   :value="rootStore.assets.auth_user.name" />
-                        <Button size="small" @click="rootStore.impersonateLogout()"
-                                severity="danger" label="Leave" />
-                    </div>
-                    <!--                <p class="text-red-500"><strong>Impersonating</strong> {{rootStore.assets.auth_user.name}}</p>-->
-                </div>
-                <div v-if="rootStore.assets.auth_user
-                && !rootStore.assets.is_impersonating"
-                     class="flex align-items-center">
-                    <a @click="toggleDropDownMenu"
-                       data-testid="Topnav-Avatar"
-                       class="cursor-pointer flex align-items-center">
-                        <Avatar :image="rootStore.assets.auth_user.avatar"
-                                class="mr-2" shape="circle" />
-                        <span>{{rootStore.assets.auth_user.name}}</span>
-                        <i class="pi pi-chevron-down text-sm mt-1 ml-1"></i>
-                    </a>
-                </div>
-                <TieredMenu  v-if="rootStore && rootStore.top_right_user_menu"
-                             :model="rootStore.top_right_user_menu"
-                             ref="menu"
-                             :popup="true"
-                >
-                </TieredMenu>
-            </template>
+            <TieredMenu  v-if="rootStore && rootStore.top_right_user_menu"
+                         :model="rootStore.top_right_user_menu"
+                         ref="menu"
+                         :popup="true"
+            >
+            </TieredMenu>
         </template>
 
     </Menubar>
