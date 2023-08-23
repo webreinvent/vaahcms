1bab4b64cdaec08cfe5b4f366ae748a1853ae13f
Pradeep <webreinvent@gmail.com>
2023-08-24 02:39:16 +0530

    Updated: getFaker method

--- Http/Controllers/Frontend/WelcomeController.php
+++ Http/Controllers/Frontend/WelcomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Str;
 use VaahCms\Modules\Cms\Entities\MenuItem;
+use WebReinvent\VaahCms\Libraries\VaahSeeder;
 use WebReinvent\VaahCms\Models\Module;
 use WebReinvent\VaahCms\Models\Theme;
 use WebReinvent\VaahCms\Models\User;
@@ -135,143 +136,7 @@ class WelcomeController extends Controller
     //----------------------------------------------------------
     public function getFaker(Request $request)
     {
-
-        $rules = array(
-            'model_namespace' => 'required',
-        );
-
-        $messages = [
-            'model_namespace.required' => "model_namespace is required. Eg: WebReinvent\VaahCms\Models\User"
-        ];
-
-        $validator = \Validator::make( $request->all(), $rules, $messages);
-        if ( $validator->fails() ) {
-
-            $errors             = errorsToArray($validator->errors());
-            $response['success'] = false;
-            $response['errors'] = $errors;
-            return $response;
-        }
-
-        $model = $request->model_namespace;
-        $model = new $model();
-
-        $table = $model->getTable();
-
-        if($request->filled('fillable'))
-        {
-            $fillable = $request->fillable;
-        } else{
-            $fillable = $model->getFillable();
-        }
-
-        if($request->filled('except'))
-        {
-            $except = $request->except;
-            $fillable = array_diff($fillable,$except);
-        };
-
-        $faker = Factory::create();
-
-        $fill = [];
-        $list = [];
-
-        $i = 0;
-        foreach ($fillable as $column)
-        {
-            $type = \DB::getSchemaBuilder()->getColumnType($table, $column);
-
-            $value = null;
-
-            switch($type)
-            {
-
-                case 'text':
-                $value = $faker->text(60);
-                    break;
-
-                case 'string':
-                    $value = $faker->text(25);
-                    break;
-
-                case 'boolean':
-                    $value = array_rand([0,1]);
-                    break;
-
-
-
-            }
-
-            $list[$i]['column'] = $column;
-            $list[$i]['type'] = $type;
-            $list[$i]['value'] = $value;
-            $fill[$column] = $value;
-            $i++;
-
-        }
-
-
-        foreach ($fill as $column => $value)
-        {
-            switch ($column){
-
-                case 'first_name':
-                    $fill[$column] = $faker->firstName;
-                    break;
-
-                case 'last_name':
-                case 'middle_name':
-                    $fill[$column] = $faker->lastName;
-                    break;
-
-                case 'alternate_email':
-                case 'email':
-                    $fill[$column] = $faker->email;
-                    break;
-
-                case 'password':
-                    $fill[$column] = $faker->password;
-                break;
-
-                case 'gender':
-                    $fill[$column] = $faker->randomElement(['m','f','o']);
-                    break;
-
-
-                case 'timezone':
-                    $fill[$column] = $faker->timezone('US');
-                break;
-
-                case 'display_name':
-                case 'username':
-                    $fill[$column] = $faker->userName;
-                    break;
-
-
-                case 'slug':
-                    if(isset($fill['name']) && !empty($fill['name']))
-                    {
-                        $fill[$column] = Str::slug($fill['name']);
-                    }
-                    break;
-
-                case 'created_by':
-                case 'updated_by':
-                    $fill[$column] = User::inRandomOrder()->first()->id;
-                    break;
-
-            }
-        }
-
-
-        $data['fill']=$fill;
-        $data['list']=$list;
-
-        $response['success'] = true;
-        $response['data'] = $data;
-        return $response;
-
-
+        return VaahSeeder::fill($request);
     }
     //----------------------------------------------------------
     //----------------------------------------------------------
