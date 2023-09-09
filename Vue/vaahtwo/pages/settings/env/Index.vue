3892d5bff55d9d292cba85ad8b9ea527f658756e
subrat <subrat.b@webreinvent.com>
2023-09-09 14:22:16 +0530

    Fixed: UI for Settings > Environment Variables

--- Vue/vaahtwo/pages/settings/env/Index.vue
+++ Vue/vaahtwo/pages/settings/env/Index.vue
@@ -31,99 +31,101 @@ onMounted(async () => {
 </script>
 
 <template>
-    <Card>
-        <template #header>
-            <div class="flex justify-content-between align-items-center">
+    <Panel class="is-small">
+        <template class="p-1" #header>
+            <div class="flex flex-row">
                 <div>
-                    <h5 class="font-semibold text-lg inline mr-2">Environment Variables</h5>
-
-                    <Tag class="mr-2">
-                        <p class="font-semibold">{{store.env_file}}</p>
-                    </Tag>
-                </div>
-                <div>
-                    <Button icon="pi pi-download"
-                            class="mr-2 p-button-sm"
-                            data-testid="env-download_file"
-                            @click="store.downloadFile(store.env_file)"
-                    />
-
-                    <Button icon="pi pi-refresh"
-                            class="p-button-sm"
-                            data-testid="-env-refresh"
-                            @click="store.sync"
-                            :loading="store.is_btn_loading"
-                    />
+                    <b class="mr-1">Environment Variables</b>
                 </div>
             </div>
         </template>
 
-        <template #content>
-            <div class="grid justify-content-start">
-                <div class="col-12 md:col-6" v-for="(item,index) in store.list">
-                    <h5 class="p-1 text-xs mb-1">{{item.key}}</h5>
-                    <form>
-                        <div class="p-inputgroup">
-                            <password v-if="store.inputType(item) == 'password'"
-                                      v-model="item.value"
-                                      :disabled="store.isDisable(item)"
-                                      toggleMask
-                                      class="p-inputtext-sm"
-                                      :data-testid="'env-'+item.key"
-                            />
+        <template #icons>
+            <div class="buttons">
+                <Button
+                        label="Download"
+                        icon="pi pi-download"
+                        class="p-button-sm mr-2"
+                        data-testid="env-download_file"
+                        @click="store.downloadFile(store.env_file)"
 
-                            <Textarea v-else
-                                      v-model="item.value"
-                                      :disabled="store.isDisable(item)"
-                                      :auto-resize="true"
-                                      :data-testid="'env-'+item.key"
-                            />
+                />
 
-                            <Button icon="pi pi-copy"
-                                    :data-testid="'env-copy_+item.key'"
-                                    @click="store.getCopy(item)"
-                            />
-
-                            <Button icon="pi pi-trash"
-                                    class="p-button-danger p-button-sm"
-                                    :data-testid="'env-remove_+item.key'"
-                                    @click="store.removeVariable(item)"
-                            />
-                        </div>
-                    </form>
-                </div>
+                <Button icon="pi pi-refresh"
+                        label="Refresh"
+                        class="p-button-sm"
+                        data-testid="env_refresh"
+                        @click="store.sync"
+                        :loading="store.is_btn_loading"
+                />
             </div>
-
-            <div class="grid justify-content-start mt-5">
-                <div class="col-12 md:col-6">
+        </template>
+        <div class="grid justify-content-start">
+            <div class="col-12 md:col-6" v-for="(item,index) in store.list">
+                <h5 class="p-1 text-xs mb-1">{{item.key}}</h5>
+                <form>
                     <div class="p-inputgroup">
-                        <InputText :autoResize="true"
-                                   v-model="store.new_variable"
-                                   class="p-inputtext-sm"
-                                   :data-testid="'env-add_variable_field'"
+                        <password v-if="store.inputType(item) == 'password'"
+                                  v-model="item.value"
+                                  :disabled="store.isDisable(item)"
+                                  toggleMask
+                                  class="p-inputtext-sm"
+                                  :data-testid="'env-'+item.key"
                         />
 
-                        <Button label="Add Env Variable"
-                                :data-testid="'env-add_variable'"
-                                icon="pi pi-plus"
-                                @click="store.addVariable"
-                                :disabled="!store.new_variable"
-                                class="p-button-sm"
+                        <Textarea v-else
+                                  v-model="item.value"
+                                  :disabled="store.isDisable(item)"
+                                  :auto-resize="true"
+                                  :data-testid="'env-'+item.key"
                         />
-                    </div>
-                </div>
 
-                <div class="col-12 md:col-6">
-                    <div class="p-inputgroup justify-content-end">
-                        <Button label="Save"
-                                icon="pi pi-save"
-                                @click="store.confirmChanges"
-                                :data-testid="'env-save_variable'"
-                                class="p-button-sm"
+                        <Button icon="pi pi-copy"
+                                :data-testid="'env-copy_+item.key'"
+                                @click="store.getCopy(item)"
+                        />
+
+                        <Button icon="pi pi-trash"
+                                class="p-button-danger p-button-sm"
+                                :data-testid="'env-remove_+item.key'"
+                                @click="store.removeVariable(item)"
                         />
                     </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="grid justify-content-start mt-1">
+            <div class="col-12 md:col-6">
+                <div class="p-inputgroup">
+                    <InputText :autoResize="true"
+                               v-model="store.new_variable"
+                               class="p-inputtext-sm"
+                               :data-testid="'env-add_variable_field'"
+                    />
+
+                    <Button label="Add Env Variable"
+                            :data-testid="'env-add_variable'"
+                            icon="pi pi-plus"
+                            @click="store.addVariable"
+                            :disabled="!store.new_variable"
+                            class="p-button-sm"
+                    />
                 </div>
             </div>
-        </template>
-    </Card>
+
+            <div class="col-12">
+                <Divider class="mb-3 mt-0"/>
+                <div class="p-inputgroup justify-content-end">
+                    <Button label="Save"
+                            icon="pi pi-save"
+                            @click="store.confirmChanges"
+                            :data-testid="'env-save_variable'"
+                            class="p-button-sm"
+                    />
+                </div>
+            </div>
+        </div>
+    </Panel>
+
 </template>
