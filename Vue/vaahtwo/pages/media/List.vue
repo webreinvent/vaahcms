97494036da14912b984d1795904ee8d8d23f57db
Kapil Singh <kapil-s001@webreinvent.com>
2023-10-20 19:29:39 +0530

    Fixed: size of create and reset buttons

--- Vue/vaahtwo/pages/media/List.vue
+++ Vue/vaahtwo/pages/media/List.vue
@@ -124,7 +124,7 @@ onMounted(async () => {
                 </template>
 
                 <template #icons>
-                    <span class="p-buttonset">
+                    <span class="p-inputgroup">
                         <Button data-testid="media-list-create"
                                 @click="store.toForm()"
                                 icon="pi pi-plus"
