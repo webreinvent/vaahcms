979999e191388db2cc8125b24a81e93bd2863e8c
Shivam Gupta <Shivam-g001@webreinvent.com>
2023-12-12 15:03:13 +0530

    Updated: allowed  media type

--- Vue/vaahtwo/stores/settings/store-general_setting.js
+++ Vue/vaahtwo/stores/settings/store-general_setting.js
@@ -478,7 +478,7 @@ export const useGeneralStore = defineStore({
             }
             else {
                 this.filtered_allowed_files = this.assets.file_types.filter((files) => {
-                    return files.toLowerCase().search(event.query.toLowerCase());
+                    return files.toLowerCase().includes(event.query.toLowerCase());
                 });
             }
         },
