6126e65a2a3c25069826768e47ce60bf4b09f171
Alam <alam-a001@webreinvent.com>
2024-01-05 16:30:43 +0530

    Updated: registrations store and controller

--- Http/Controllers/Backend/RegistrationsController.php
+++ Http/Controllers/Backend/RegistrationsController.php
@@ -73,6 +73,17 @@ class RegistrationsController extends Controller
             $data['fields'] = User::getUserSettings();
             $data['custom_fields'] = $custom_fields;
 
+            //---------------------------------------------
+            $data['language_string'] = [
+                "registrations_title" => trans("vaahcms-registration.registrations_title"),
+                "filter_email_verification_pending" => trans("vaahcms-registration.filter_email_verification_pending"),
+                "filter_email_verified" => trans("vaahcms-registration.filter_email_verified"),
+                "filter_user_created" => trans("vaahcms-registration.filter_user_created"),
+                "table_gender_male" => trans("vaahcms-registration.table_gender_male"),
+                "table_gender_female" => trans("vaahcms-registration.table_gender_female"),
+                "table_gender_others" => trans("vaahcms-registration.table_gender_others"),
+            ];
+
             $response['success'] = true;
             $response['data'] = $data;
         } catch (\Exception $e) {
