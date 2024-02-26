a58c78b09bae91b0d87b3e6d9e2fdbf47dfc0c17
Shivam Gupta <Shivam-g001@webreinvent.com>
2024-02-26 11:12:40 +0530

    Updated: Advance layout menu

--- Vue/vaahtwo/pages/advanced/AdvancedLayout.vue
+++ Vue/vaahtwo/pages/advanced/AdvancedLayout.vue
@@ -1,7 +1,8 @@
 <script setup>
 import {onMounted, ref, watch} from "vue";
 import {useRoute} from 'vue-router';
-
+import {useLogStore} from "../../stores/advanced/store-logs";
+const store = useLogStore();
 const route = useRoute();
 
 const menu_pt = ref({
@@ -10,34 +11,47 @@ const menu_pt = ref({
     })
 });
 
-const sidebar_menu_items = ref([
-    {
-        label: 'ADVANCED',
-        items: [
-            {
-                label: 'Logs',
-                icon: 'pi pi-book',
-                route: '/vaah/advanced/logs'
-            },
-            {
-                label: 'Jobs',
-                icon: 'pi pi-align-justify',
-                route: '/vaah/advanced/jobs'
-            },
-            {
-                label: 'Failed Jobs',
-                icon: 'pi pi-times-circle',
-                route: '/vaah/advanced/failedjobs'
-            },
-            {
-                label: 'Batches',
-                icon: 'pi pi-server',
-                route: '/vaah/advanced/batches'
-            }
-        ]},
-]);
 
+
+const advanceValue = ref('');
+const sidebar_menu_items = ref([]);
+
+const updateSidebarMenuItems = (languageStrings) => {
+    sidebar_menu_items.value = [
+        {
+            label: languageStrings.advanced,
+            items: [
+                {
+                    label: languageStrings.logs,
+                    icon: 'pi pi-book',
+                    route: '/vaah/advanced/logs'
+                },
+                {
+                    label: languageStrings.jobs,
+                    icon: 'pi pi-align-justify',
+                    route: '/vaah/advanced/jobs'
+                },
+                {
+                    label: languageStrings.failed_jobs,
+                    icon: 'pi pi-times-circle',
+                    route: '/vaah/advanced/failedjobs'
+                },
+                {
+                    label: languageStrings.batches,
+                    icon: 'pi pi-server',
+                    route: '/vaah/advanced/batches'
+                }
+            ]
+        },
+    ];
+};
+
+watch(() => store.assets, (newAssets) => {
+    advanceValue.value = newAssets.language_strings.advanced;
+    updateSidebarMenuItems(newAssets.language_strings);
+});
 onMounted(async () => {
+    updateSidebarMenuItems(store.assets.language_strings);
 
 });
 
