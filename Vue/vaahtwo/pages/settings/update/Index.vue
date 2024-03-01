ee101417b91ae20e2d363d87ff17d9cc9a36333c
Shivam Gupta <Shivam-g001@webreinvent.com>
2024-03-01 15:41:52 +0530

    Updated: language strings for update vaah

--- Vue/vaahtwo/pages/settings/update/Index.vue
+++ Vue/vaahtwo/pages/settings/update/Index.vue
@@ -22,7 +22,7 @@ onMounted(async () => {
 });
 </script>
 <template>
-    <Panel class="is-small">
+    <Panel class="is-small" v-if="root && root.assets">
         <template class="p-1" #header>
             <div class="flex flex-row">
                 <div>
@@ -34,7 +34,7 @@ onMounted(async () => {
         <template #icons>
             <div class="buttons">
                 <Button icon="pi pi-refresh"
-                        label="Check for Update"
+                        :label="root.assets.language_strings.update.check_for_update_button"
                         data-testid="setting-update_check"
                         @click="store.checkForUpdate"
                         class="p-button-sm"></Button>
@@ -52,7 +52,7 @@ onMounted(async () => {
 
                         class="w-auto my-2 p-button-sm"
                 />
-                <p v-if="store.is_up_to_data"><span class="subtitle">Current version of this VaahCMS is the latest version</span></p>
+                <p v-if="store.is_up_to_data"><span class="subtitle">{{root.assets.language_strings.update.check_for_update_message}}</span></p>
             </div>
         </Message>
 
@@ -60,14 +60,14 @@ onMounted(async () => {
             <div v-if="store.release" class="text-sm">
                 <Message class="py-2" icon="pi-sync pi"
                     severity="success" :closable="false">
-                    <p>A newer version <b>{{ store.remote_version }}</b> of VaahCMS is available.</p>
+                    <p>{{root.assets.language_strings.update.a_newer_version}}<b>{{ store.remote_version }}</b> {{root.assets.language_strings.update.of_vaahcms_is_available}}</p>
                 </Message>
 
                 <Panel class="is-small">
                     <template class="p-1" #header>
                         <div class="flex flex-row">
                             <div>
-                                <b class="mr-1">NEW Updates:</b>
+                                <b class="mr-1">{{root.assets.language_strings.update.new_updates}}:</b>
                             </div>
                         </div>
                     </template>
@@ -81,7 +81,7 @@ onMounted(async () => {
                                   @input="store.is_button_active = true"
                                   :binary="true"
                                   class="is-small"/>
-                        <label for="binary">Have you taken the backup of your files & database?</label>
+                        <label for="binary">{{root.assets.language_strings.update.new_updates_message}}</label>
                     </div>
                     <Button label="Update Now"
                             :disabled="!store.is_button_active"
@@ -92,7 +92,7 @@ onMounted(async () => {
                         <div class="col-3">
                             <ol class="pl-3">
                                 <li class="mb-2">
-                                    Downloading latest version (it can take upto 3 to 5 minutes)
+                                    {{root.assets.language_strings.update_vaah.downloading_latest_version}}
                                     <i v-if="store.status.download_latest_version === 'success'"
                                        class="pi pi-check"></i>
                                     <i v-else-if="store.status.download_latest_version === 'pending'"
