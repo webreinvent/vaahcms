135fad9977a2cf99a130a971b83c528065495677
Vikram Chand <vikram.wri231@webreinvent.com>
2024-03-19 18:23:17 +0530

    Fixed: user trash method

--- Models/User.php
+++ Models/User.php
@@ -443,17 +443,13 @@ class User extends UserBase
                     ->update(['is_active' => null]);
                 break;
             case 'trash':
-                self::where('id', $id)
-                    ->withTrashed()
+                self::find($id)
                     ->delete();
-                $item = self::where('id',$id)->withTrashed()->first();
-                $item->deleted_by = auth()->user()->id;
-                $item->save();
                 break;
             case 'restore':
                 self::where('id', $id)
-                    ->withTrashed()
-                    ->restore();
+                    ->onlyTrashed()
+                    ->first()->restore();
                 break;
             case 'generate-new-token':
 
