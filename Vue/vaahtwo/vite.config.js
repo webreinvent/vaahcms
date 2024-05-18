b27d9785dffa215228ea525d79e4e547dfae5195
Vikram Chand <vikram.wri231@webreinvent.com>
2024-05-18 13:31:50 +0530

    Updated: prod mismatch hydration true for PROD

--- Vue/vaahtwo/vite.config.js
+++ Vue/vaahtwo/vite.config.js
@@ -48,5 +48,9 @@ export default defineConfig({
             host: 'localhost',
 
         }
+    },
+    define: {
+        // enable hydration mismatch details in production build
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
     }
 })
