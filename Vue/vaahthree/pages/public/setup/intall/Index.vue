5296e583e95f36f443ac46995b842a4a08c160d4
Pradeep <webreinvent@gmail.com>
2024-10-30 02:36:27 +0530

    updated: layout

--- Vue/vaahthree/pages/public/setup/intall/Index.vue
+++ Vue/vaahthree/pages/public/setup/intall/Index.vue
@@ -21,12 +21,37 @@ onMounted(async () => {
 
 <template>
     <div v-if="store && store.assets && root && root.assets" class="">
+
         <div class="text-center mb-4">
-            <img v-if="root.assets.backend_logo_url"
-                 :src="root.assets.backend_logo_url" alt=""
-                 class="mb-2 mx-auto h-3rem">
+
             <h4 class="text-xl font-semibold">Install VaahCMS</h4>
         </div>
+
+
+
+
+        <ol class="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
+            <li class="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+        <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+            <svg class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+            </svg>
+            Personal <span class="hidden sm:inline-flex sm:ms-2">Info</span>
+        </span>
+            </li>
+            <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+        <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+            <span class="me-2">2</span>
+            Account <span class="hidden sm:inline-flex sm:ms-2">Info</span>
+        </span>
+            </li>
+            <li class="flex items-center">
+                <span class="me-2">3</span>
+                Confirmation
+            </li>
+        </ol>
+
+
         <div class="container vh-step relative">
             <Steps
                 :model="store.install_items"
