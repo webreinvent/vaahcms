3be42a3e201879d6a5585d135f05f17eab827dab
Vikram Chand <vikram.wri231@webreinvent.com>
2025-11-04 15:29:25 +0530

    Updated: default datetime format

--- Models/ThemeTemplate.php
+++ Models/ThemeTemplate.php
@@ -46,7 +46,7 @@ class ThemeTemplate extends VaahModel {
     {
         $date_time_format = config('settings.global.datetime_format');
 
-        return $date->format($date_time_format);
+        return $date->format($date_time_format ?? 'Y-m-d H:i:s');
 
     }
 
